3d93afd27eb5926c14daffacfcfc8b6c5da64a53
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-03-16 22:05:08 +0000

    DAM Classes

---
+++ 999_web/trunk/data/agent/CustomerDAM.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package agentDAM
+ */
+
+/**
+ * Class that provides access to the database.
+ *
+ */
+class CustomerDAM{
+	/**
+	 * Returns Customer instance object if a match for the nit providad was found in database.
+	 *
+	 * @param string $nit
+	 * @return Customer
+	 */
+	static public function getInstance($nit){
+		//Code here
+	}
+}
+?>
