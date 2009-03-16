250ba988c1b24e2ed9cd22dc2a1759273a72aab6
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-03-16 21:17:56 +0000

    Renames

---
+++ 999_web/trunk/business/agent/Agent.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @package agent
+ */
+
+/**
+ * Defines common functionality for agents derived classes.
+ * @package agent
+ * @author Roberto Oliveros
+ */
+abstract class Agent{
+	/**
+	 * Nit (Numero de Identificacion Tributaria) of the agent.
+	 * @var string
+	 */
+	private $_mNit;
+	
+	/**
+	 * Name for the agent.
+	 *
+	 * @var string
+	 */
+	private $_mName;
+	
+	/**
+	 * Status of the object instance, e.g. unsaved = 0, saved = 1.
+	 *
+	 * @var unknown_type
+	 */
+	private $_mStatus;
+	
+	/**
+	 * Agent constructor function. Receives the status for the created instance object. If created from database,
+	 * the status must be set to 1, otherwise set to 0. Due to the lack of experience... sorry.
+	 *
+	 * @param integer $status
+	 */
+	public function __construct($status){
+		$this->_mStatus = $status;
+	}
+	
+	/**
+	 * Returns the nit of the agent.
+	 *
+	 * @return string
+	 */
+	public function getNit(){
+		return $this->_mNit;
+	}
+	
+	/**
+	 * Returns the name of the agent.
+	 *
+	 * @return string
+	 */
+	public function getName(){
+		return $this->_mName;
+	}
+	
+	/**
+	 * Sets the name of the agent.
+	 *
+	 * @param string $name
+	 * @return void
+	 */
+	public function setName($name){
+		if(empty($name))
+			throw new Exception('Name is empty.');
+			
+		$this->_mName = $name;
+	}
+	
+	/**
+	 * Saves agent's data to the database. Nit and Name must be set.
+	 * 
+	 * @return void
+	 */
+	public function save(){
+		if(empty($this->_mNit))
+			throw new Exception('Nit is empty.');
+			
+		if(empty($this->_mName))
+			throw new Exception('Name is empty.');
+	}
+}
+?>
---
+++ 999_web/trunk/business/agent/Customer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package agent
+ */
+require_once('Agent.php');
+
+/**
+ * Defines functionality for the customer class used for invoices.
+ * @package agent
+ * @author Roberto Oliveros
+ */
+class Customer extends Agent{
+	
+}
+?>
