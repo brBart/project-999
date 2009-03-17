59165f47829a5b607330e731b2899a014533b8c2
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-03-17 01:41:43 +0000

    Adding configuration file.

---
+++ 999_web/trunk/include/config.php
@@ -0,0 +1,8 @@
+<?php
+// SITE_ROOT contains the full path to the proyect's root
+define('SITE_ROOT', dirname(dirname(__FILE__)));
+
+// Libraries
+define('BUSINESS_DIR', SITE_ROOT . '/business/');
+define('DATA_DIR', SITE_ROOT . '/data/');
+?>
