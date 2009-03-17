13dd31aa4243112a281a2dbdfae2e8f3b77d17c2
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-03-17 01:50:44 +0000

    Reestructur.

--- 999_web/trunk/business/agent.php
+++ 999_web/trunk/business/agent.php
@@ -2,8 +2,91 @@
 /**
  * @package agent
  */
-require_once('Agent.php');
-require_once('../../data/agent/CustomerDAM.php');
+
+require_once('../include/config.php');
+require_once DATA_DIR . 'agent_dam.php';
+
+/**
+ * Defines common functionality for agents derived classes.
+ * @package agent
+ * @author Roberto Oliveros
+ */
+abstract class Agent{
+	/**
+	 * Nit (Numero de Identificacion Tributaria) of the agent.
+	 * @var string
+	 */
+	protected $_mNit;
+	
+	/**
+	 * Name for the agent.
+	 *
+	 * @var string
+	 */
+	protected $_mName;
+	
+	/**
+	 * Status of the object instance, e.g. unsaved = 0, saved = 1.
+	 *
+	 * @var unknown_type
+	 */
+	protected $_mStatus;
+	
+	/**
+	 * Agent constructor method. Receives the status for the created instance object. If created from database,
+	 * the status must be set to 1, otherwise set to 0 (default = 0). Due to the lack of experience... sorry.
+	 *
+	 * @param integer $status
+	 */
+	public function __construct($status = 0){
+		$this->_mStatus = $status;
+	}
+	
+	/**
+	 * Returns the nit of the agent.
+	 *
+	 * @return string
+	 */
+	public function getNit(){
+		return $this->_mNit;
+	}
+	
+	/**
+	 * Returns the name of the agent.
+	 *
+	 * @return string
+	 */
+	public function getName(){
+		return $this->_mName;
+	}
+	
+	/**
+	 * Sets the name of the agent.
+	 *
+	 * @param string $name
+	 * @return void
+	 */
+	public function setName($name){
+		if(empty($name))
+			throw new Exception('Name is empty.');
+			
+		$this->_mName = $name;
+	}
+	
+	/**
+	 * Saves agent's data to the database. Nit and Name must be set.
+	 * 
+	 * @return void
+	 */
+	public function save(){
+		if(empty($this->_mNit))
+			throw new Exception('Nit is empty.');
+			
+		if(empty($this->_mName))
+			throw new Exception('Name is empty.');
+	}
+}
+
 
 /**
  * Defines functionality for the customer class used for invoices.
