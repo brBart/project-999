54a501807e77bb84e9ae910c7fb69985edb5e99d
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-03-23 16:18:13 +0000

    Finished adding dummy data.

--- 999_web/trunk/data/document_search_dam.php
+++ 999_web/trunk/data/document_search_dam.php
@@ -283,4 +283,84 @@ class ReceiptSearchDAM{
 		return $data_array;
 	}
 }
+
+
+/**
+ * Defines functionality for searching the database for Entry Inventory Adjustment Documents.
+ * @package DocumentSearchDAM
+ * @author Roberto Oliveros
+ */
+class EntryIASearchDAM{
+	/**
+	 * Returns an array with the found data in the database. The array consists of the 2 fields(Date & Id) which
+	 * is the date when the document was created and its respective id.
+	 *
+	 * @param string $startDate
+	 * @param string_type $endDate
+	 * @param integer $page
+	 * @param integer $totalPages
+	 * @param integer $totaItems
+	 * @return array
+	 */
+	static public function search($startDate, $endDate, $page, &$totalPages, &$totaItems){
+		$data_array = array();
+		
+		if($page == 1){
+			$data_array[] = array('date' => '10/01/2009', 'id' => '123');
+			$data_array[] = array('date' => '11/01/2009', 'id' => '124');
+			$data_array[] = array('date' => '12/01/2009', 'id' => '125');
+			$data_array[] = array('date' => '13/01/2009', 'id' => '126');
+			$data_array[] = array('date' => '14/01/2009', 'id' => '127');
+		}
+		else{
+			$data_array[] = array('date' => '15/01/2009', 'id' => '128');
+			$data_array[] = array('date' => '16/01/2009', 'id' => '129');
+		}
+		
+		$totalPages = 2;
+		$totaItems = 7;
+		
+		return $data_array;
+	}
+}
+
+
+/**
+ * Defines functionality for searching the database for Withdraw Inventory Adjustment Documents.
+ * @package DocumentSearchDAM
+ * @author Roberto Oliveros
+ */
+class WithdrawIASearchDAM{
+	/**
+	 * Returns an array with the found data in the database. The array consists of the 2 fields(Date & Id) which
+	 * is the date when the document was created and its respective id.
+	 *
+	 * @param string $startDate
+	 * @param string_type $endDate
+	 * @param integer $page
+	 * @param integer $totalPages
+	 * @param integer $totaItems
+	 * @return array
+	 */
+	static public function search($startDate, $endDate, $page, &$totalPages, &$totaItems){
+		$data_array = array();
+		
+		if($page == 1){
+			$data_array[] = array('date' => '10/01/2009', 'id' => '123');
+			$data_array[] = array('date' => '11/01/2009', 'id' => '124');
+			$data_array[] = array('date' => '12/01/2009', 'id' => '125');
+			$data_array[] = array('date' => '13/01/2009', 'id' => '126');
+			$data_array[] = array('date' => '14/01/2009', 'id' => '127');
+		}
+		else{
+			$data_array[] = array('date' => '15/01/2009', 'id' => '128');
+			$data_array[] = array('date' => '16/01/2009', 'id' => '129');
+		}
+		
+		$totalPages = 2;
+		$totaItems = 7;
+		
+		return $data_array;
+	}
+}
 ?>
