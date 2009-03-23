e8a542169027efdc69f70d172b0801109cecf0d1
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-03-23 17:40:57 +0000

    First commit and addition of bankDAM class.

---
+++ 999_web/trunk/data/cash_dam.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Library with the utility database classes for the Cash package.
+ * @package CashDAM
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Defines functionality for accessing the bank's database tables.
+ * @package CashDAM
+ * @author Roberto Oliveros
+ */
+class BankDAM{
+	/**
+	 * Returns a Bank if it founds an id match in the database. Otherwise returns NULL.
+	 *
+	 * @param integer $id
+	 * @return Bank
+	 */
+	static public function getInstance($id){
+		if($id == 123){
+			$bank = new Bank(123, FROM_DATABASE);
+			$bank->setData('GyT Continental');
+			return $bank;
+		}
+		else
+			return NULL;
+	}
+	
+	/**
+	 * Insert a Bank in the database.
+	 *
+	 * @param Bank $obj
+	 * @return void
+	 */
+	static public function insert(Bank $obj){
+		// Code here...
+	}
+	
+	/**
+	 * Updates a Bank data in the database.
+	 *
+	 * @param Bank $obj
+	 * @return void
+	 */
+	static public function update(Bank $obj){
+		// Code here...
+	}
+	
+	/**
+	 * Deletes a Bank from the datase. Returns true on success, otherwise it has dependencies and returns false.
+	 *
+	 * @param Bank $obj
+	 * @return boolean
+	 */
+	static public function delete(Bank $obj){
+		if($obj->getId() == 123)
+			return true;
+		else
+			return false;
+	}
+}
+?>
