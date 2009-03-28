1423533ebacc17ecc1e9b7e859972ac94907b409
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-03-28 18:09:04 +0000

    First commit and addition of RoleDAM class.

---
+++ 999_web/trunk/data/user_account_dam.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Utility library including classes for accessing the database tables.
+ * @package UserAccountDAM
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Defines functionality for accessing the role's database tables.
+ * @package UserAccountDAM
+ * @author Roberto Oliveros
+ */
+class RoleDAM{
+	/**
+	 * Returns instance of a role.
+	 *
+	 * Returns NULL if there was no match for the provided id in the database.
+	 * @param integer $id
+	 * @return Role
+	 */
+	static public function getInstance($id){
+		if($id == 123){
+			$role = new Role($id, 'Administrador');
+			return $role;
+		}
+		else
+			return NULL;
+	}
+}
+?>
