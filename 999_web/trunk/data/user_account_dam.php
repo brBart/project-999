299ddd01fda9c1201071a6d6499382632bb708af
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-03-31 16:13:01 +0000

    Dummy routines.

--- 999_web/trunk/data/user_account_dam.php
+++ 999_web/trunk/data/user_account_dam.php
@@ -35,6 +35,67 @@ class RoleDAM{
  * @author Roberto Oliveros
  */
 class UserAccountDAM{
+	/**
+	 * Verifies if the account's name already exists in the database.
+	 *
+	 * @param string $userName
+	 * @return boolean
+	 */
+	static public function exists($userName){
+		if($userName == 'roboli')
+			return true;
+		else
+			return false;
+	}
 	
+	/**
+	 * Retuns instance of a user account.
+	 *
+	 * Returns NULL if there was no match fot the provided username in the database.
+	 * @param string $userName
+	 * @return UserAccount
+	 */
+	static public function getInstance($userName){
+		if($userName == 'roboli'){
+			$user_account = new UserAccount($userName, PersistObject::CREATED);
+			$role = Role::getInstance(123);
+			$user_account->setData('Roberto', 'Oliveros', $role,false);
+			return $user_account;
+		}
+		else
+			return NULL;
+	}
+	
+	/**
+	 * Inserts the user account's data in the database.
+	 *
+	 * @param UserAccount $obj
+	 */
+	static public function insert(UserAccount $obj){
+		// Code here...
+	}
+	
+	/**
+	 * Updates the user account's data in the database.
+	 *
+	 * @param UserAccount $obj
+	 */
+	static public function update(UserAccount $obj){
+		// Code here...
+	}
+	
+	/**
+	 * Deletes the user account from the database.
+	 *
+	 * Returns true on success, otherwise false due dependencies.
+	 * @param UserAccount $obj
+	 * @return boolean
+	 */
+	static public function delete(UserAccount $obj){
+		if($obj->getUserName() == 'roboli')
+			return true;
+		else
+			return false;
+	}
 }
 ?>
