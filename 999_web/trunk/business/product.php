08bde10f95ed3b1ded1bf6996c5c501db805809c
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-04-03 15:35:52 +0000

    Working on Inventory class getNegativeLots method.

--- 999_web/trunk/business/product.php
+++ 999_web/trunk/business/product.php
@@ -167,7 +167,62 @@ class Inventory{
 		return InventoryDAM::getQuantity($this->_mProduct);
 	}
 	
+	/**
+	 * Returns the necessary lots to fulfill the requested quantity of units.
+	 *
+	 * Returns an array containing the necessary lots with available quantity to fulfill the requested quantity.
+	 * @param integer $reqUnitsQuantity
+	 * @return array<Lot>
+	 */
+	public function getLots($reqUnitsQuantity){
+		// Get the lots from the database with available stock.
+		$in_stock_lots = InventoryDAM::getLots($this->_mProduct);
+		// The returned qrray with the lots which fulfill the requested quantity of units.
+		$lots = array();
+
+		$lot = current($in_stock_lots);
+		do{
+			// Verify the available quantity of the lot.
+			if(!$lot)
+				$available = 0;
+			else
+				$available = $lot->getAvailable();
+
+			// If there is available quantity but doesn't fulfill the requested quantity.
+			if($available > 0 && $available < $reqUnitsQuantity){
+				$lots[] = $lot;
+				$reqUnitsQuantity = $reqUnitsQuantity - $available;
+			// If the available quantity does fulfill the requested quantity.
+			}elseif($available > 0 && $available >= $reqUnitsQuantity){
+				$lots[] = $lot;
+				$reqUnitsQuantity = 0;
+			// Otherwise create a new empty lot.
+			}else {
+				$new_lot = new Lot($this->_mProduct, 0, 0);
+				$this->addLot($new_lot);
+				$lots[] = $new_lot;
+				$reqUnitsQuantity = 0;
+			}
+			
+			$lot = next($in_stock_lots);
+		} while($reqUnitsQuantity > 0);
+		
+		return $lots;
+	}
 	
 	
+	public function getNegativeLots($reqUnitsQuantity){
+		// Get the negative lots from the database.
+		$negative_lots = InventoryDAM::getNegativeLots($this->_mProduct);
+		// The returned array with the negative lots.
+		$lots = array();
+		
+		$lot = current($negative_lots);
+		do{
+			if(!$lot){
+				
+			}
+		} while($reqUnitsQuantity > 0);
+	}
 }
 ?>
