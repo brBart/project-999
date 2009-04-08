f046ff8bab22ecd383cd7d5cda4172f6d984bc00
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-04-08 00:24:59 +0000

    Adding dummy routines.

--- 999_web/trunk/data/product_dam.php
+++ 999_web/trunk/data/product_dam.php
@@ -125,4 +125,62 @@ class ManufacturerDAM{
 			return false;
 	}
 }
+
+
+/**
+ * Class in charge of accessing the database tables regarding the product's details.
+ * @package ProductDAM
+ * @author Roberto Oliveros
+ */
+class ProductDetailDAM{
+	/**
+	 * Inserts the product detail's data into the database.
+	 *
+	 * @param Product $product
+	 * @param ProductDetail $detail
+	 */
+	static public function insert(Product $product, ProductDetail $detail){
+		// Code here...
+	}
+	
+	/**
+	 * Deletes the product detail from the database.
+	 *
+	 * @param Product $product
+	 * @param ProductDetail $detail
+	 */
+	static public function delete(Product $product, ProductDetail $detail){
+		// Code here...
+	}
+}
+
+
+/**
+ * Class responsible for accessing the product's tables in the database.
+ * @package ProductDAM
+ * @author Roberto Oliveros
+ */
+class ProductDAM{
+	/**
+	 * Returns an instance of a product.
+	 *
+	 * Returns NULL in case there was no match for the provided id in the database.
+	 * @param integer $id
+	 * @return Product
+	 */
+	static public function getInstance($id){
+		if($id == 123){
+			$product = new Product($id, Persist::CREATED);
+			$um = UnitOfMeasure::getInstance(123);
+			$manufacturer = Manufacturer::getInstance(123);
+			$details = array();
+			$details[] = new ProductDetail(Supplier::getInstance(123), 'Abb213', Persist::CREATED);
+			$product->setData('Pepto Bismol', '12345', '120 ml', 'Para dolores de estomagol.', $um,
+					$manufacturer, 12.65, false, $details);
+			return $product;
+		}
+		else
+			return NULL;
+	}
+}
 ?>
