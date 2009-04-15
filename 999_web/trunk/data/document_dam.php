6b8e142898be93e894a6605490297a2f2c0715eb
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-04-15 00:28:56 +0000

    First commit. Dummy routines.

---
+++ 999_web/trunk/data/document_dam.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Library with utility classes for accessing database tables regarding documents.
+ * @package DocumentDAM
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Class for accessing document bonus detail tables.
+ * @package DocumentDAM
+ * @author Roberto Oliveros
+ */
+class DocBonusDetailDAM{
+	/**
+	 * Inserts the detail's data into the database.
+	 *
+	 * @param DocBonusDetail $detail
+	 * @param Document $doc
+	 * @param integer $number
+	 */
+	static public function insert(DocBonusDetail $detail, Document $doc, $number){
+		// Code here...
+	}
+}
+?>
