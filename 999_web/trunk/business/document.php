a9217047f8efedff4e4aee98c1775441c07edc03
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-04-15 00:29:13 +0000

    Fixes.

--- 999_web/trunk/business/document.php
+++ 999_web/trunk/business/document.php
@@ -6,6 +6,15 @@
  */
 
 /**
+ * For persistence needs.
+ */
+require_once('business/persist.php');
+/**
+ * For accessing the database.
+ */
+require_once('data/document_dam.php');
+
+/**
  * Defines common functionality for all the document derived classes.
  * @package Document
  * @author Roberto Oliveros
@@ -25,20 +34,20 @@ abstract class Document extends PersistDocument{
  * @package Document
  * @author Roberto Oliveros
  */
-abstract class Detail{
+abstract class DocumentDetail{
 	/**
 	 * Holds the detail's quantity.
 	 *
 	 * @var integer
 	 */
-	private $_mQuantity;
+	protected $_mQuantity;
 	
 	/**
 	 * Holds the detail's item price.
 	 *
 	 * @var float
 	 */
-	private $_mPrice;
+	protected $_mPrice;
 	
 	/**
 	 * Constructs the deatail with the provided quantity and price.
@@ -83,6 +92,20 @@ abstract class Detail{
 	}
 	
 	/**
+	 * Saves the detail's data in the database.
+	 *
+	 * The document is where this detail belongs to. The number parameter is necessary to keep the order in
+	 * which all the details where created.
+	 * @param Document $doc
+	 * @param integer $number
+	 */
+	public function save(Document $doc, $number){
+		Persist::validateObjectFromDatabase($doc);
+		$this->validateNumber($number);
+		$this->insert($doc, $number);
+	}
+	
+	/**
 	 * Returns the detail's id.
 	 * @return string
 	 */
@@ -97,16 +120,6 @@ abstract class Detail{
 	abstract public function show();
 	
 	/**
-	 * Saves the detail's data in the database.
-	 *
-	 * The document is where this detail belongs to. The number parameter is necessary to keep the order in
-	 * which all the details where created.
-	 * @param Document $document
-	 * @param integer $number
-	 */
-	abstract public function save(Document $document, $number);
-	
-	/**
 	 * Undoes every action previously taken.
 	 *
 	 */
@@ -117,6 +130,14 @@ abstract class Detail{
 	 * @return boolean
 	 */
 	abstract public function isCancellable();
+	
+	/**
+	 * Inserts the detail's data in the database.
+	 *
+	 * @param Document $doc
+	 * @param integer $number
+	 */
+	abstract protected function insert(Document $doc, $number);
 	
 	/**
 	 * Validates the detail's quantity.
@@ -138,6 +159,108 @@ abstract class Detail{
 	private function validatePrice($price){
 		if(!is_float($price) || $price < 0)
 			throw new Exception('Precio inv&accute;lido.');
+	}
+	
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be greater than cero. Otherwise it throws an exception.
+	 * @param integer $number
+	 */
+	private function validateNumber($number){
+		if(!is_int($number) || $number < 1)
+			throw new Exception('Internal error, number invalid!');
+	}
+}
+
+
+/**
+ * Represents a document detail with a bonus.
+ * @package Document
+ * @author Roberto Oliveros
+ */
+class DocBonusDetail extends DocumentDetail{
+	/**
+	 * Holds the detail's bonus.
+	 *
+	 * @var Bonus
+	 */
+	private $_mBonus;
+	
+	/**
+	 * Constructs the detail with the provided data.
+	 *
+	 * @param Bonus $bonus
+	 * @param float $price
+	 */
+	public function __construct(Bonus $bonus, $price){
+		parent::__construct(1, $price);
+		
+		Persist::validateObjectFromDatabase($bonus);
+		$this->_mBonus = $bonus;
+	}
+	
+	/**
+	 * Returns the id of the detail.
+	 *
+	 * @return string
+	 */
+	public function getId(){
+		return 'bon-' . $this->_mBonus->getProduct()->getId();
+	}
+	
+	/**
+	 * Returns the detail's bonus.
+	 *
+	 * @return Bonus
+	 */
+	public function getBonus(){
+		return $this->_mBonus;
+	}
+	
+	/**
+	 * Returns an array with the detail's data.
+	 *
+	 * The array contains the fields id, product, packaging, quantity, price, total. All the others fields
+	 * are blank.
+	 * @return array
+	 */
+	public function show(){
+		$product = $this->_mBonus->getProduct();
+		
+		return array('id' => $this->getId(), 'bar_code' => '', 'manufacturer' => '',
+				'product' => $product->getName(), 'packaging' => $product->getPackaging(),
+				'um' => '', 'quantity' => $this->_mQuantity, 'price' => $this->_mPrice,
+				'total' => $this->getTotal(), 'expiration_date' => '');
+	}
+	
+	/**
+	 * Does nothing, just to fulfill the abstraction.
+	 *
+	 */
+	public function cancel(){
+		// Do nothing.
+	}
+	
+	/**
+	 * Returns true. Always can be cancellable.
+	 *
+	 * @return boolean
+	 */
+	public function isCancellable(){
+		return true;
+	}
+	
+	/**
+	 * Inserts the detail's data in the database.
+	 *
+	 * The document is where this detail belongs to. The number parameter is necessary to keep the order in
+	 * which all the details where created.
+	 * @param Document $doc
+	 * @param integer $number
+	 */
+	protected function insert(Document $doc, $number){
+		DocBonusDetailDAM::insert($this, $doc, $number);
 	}
 }
 ?>
