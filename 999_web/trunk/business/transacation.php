a2947d5a0c61e17d586582e109ce07b7f23e6de7
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-04-17 18:15:08 +0000

    First commit.

---
+++ 999_web/trunk/business/transacation.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Library with the necessary classes for commiting an inventory transaction.
+ * @package Transaction
+ * @author Roberto Oliveros
+ */
+
+
+/**
+ * Defines common functionality for a transaction class.
+ * @package Transaction
+ * @author Roberto Oliveros
+ */
+class Transaction{
+	
+}
+?>
