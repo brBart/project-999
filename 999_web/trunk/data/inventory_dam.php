91bd2a5a23566ae6edd0e1bcab4e2062be96124e
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-05-13 02:23:05 +0000

    First commit and addition of the ComparisonDAM class with dummy routines.

---
+++ 999_web/trunk/data/inventory_dam.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Library with utility classes for accessing inventory related tables in the database.
+ * @package InventoryDAM
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Utility class for accessing comparison data in the database.
+ * @package InventoryDAM
+ * @author Roberto Oliveros
+ */
+class ComparisonDAM{
+	/**
+	 * Returns a comparison with the details corresponding to the requested page.
+	 *
+	 * The total_pages and total_items arguments are necessary to return their respective values. Returns NULL
+	 * if there was no match for the provided id in the database. 
+	 * @param integer $id
+	 * @param integer $total_pages
+	 * @param integer $total_items
+	 * @param integer $page
+	 * @return Comparison
+	 */
+	static public function getInstance($id, &$total_pages, &$total_items, $page){
+		switch($id){
+			case 123:
+				$details[] = new ComparisonDetail(Product::getInstance(125), 10, 10);
+				$comparison = new Comparison($id, '01/05/2009', UserAccount::getInstance('roboli'), 'Los hay.',
+						false, $details, 10, 10);
+				return $comparison;
+				break;
+				
+			default:
+				return NULL;
+		}
+	}
+}
+?>
