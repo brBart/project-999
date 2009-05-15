f90130837c75abdf81f10d01261013e43b4b5a6b
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-05-15 19:14:02 +0000

    First commit and addition of the DataList and BankList classes.

---
+++ 999_web/trunk/business/list.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Library with utility classes for obtaining data lists of any of the system's subjects.
+ * @package List
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Defines the interface for the rest of the classes that extend it.
+ * @package List
+ * @author Roberto Oliveros
+ */
+abstract class DataList{
+	/**
+	 * Defines the method signature.
+	 *
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	abstract static public function getList($page = 0, &$total_pages = 0, &$total_items = 0);
+}
+
+
+/**
+ * Utility class for obtaining a list of banks from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class BankList extends DataList{
+	/**
+	 * Returns an array with the banks' names from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page);
+			
+		return BankListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+?>
