8afe2a10170906d1294bef96514c3b385d9a61fc
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-05-16 00:33:50 +0000

    Addition of the list classes.

--- 999_web/trunk/business/list.php
+++ 999_web/trunk/business/list.php
@@ -55,4 +55,329 @@ class BankList extends DataList{
 		return BankListDAM::getList($page, $total_pages, $total_items);
 	}
 }
+
+
+/**
+ * Utility class for obtaining a list of pending deposits from confirmation from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class PendingDepositList extends DataList{
+	/**
+	 * Returns an array with the deposits' date, id, number, bank_account, bank and amount from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return PendingDepositListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of manufacturers from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class ManufacturerList extends DataList{
+	/**
+	 * Returns an array with the manufacturers' id and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return ManufacturerListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of correlatives from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class CorrelativeList extends DataList{
+	/**
+	 * Returns an array with the correlatives' serial_number and default flag from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return CorrelativeListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of bank accounts from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class BankAccountList extends DataList{
+	/**
+	 * Returns an array with the bank accounts' number and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return BankAccountListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of user accounts from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class UserAccountList extends DataList{
+	/**
+	 * Returns an array with the user accounts' username and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return UserAccountListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of payment card brands from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class PaymentCardBrandList extends DataList{
+	/**
+	 * Returns an array with the payment card brands' id and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return PaymentCardBrandListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of products from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class ProductList extends DataList{
+	/**
+	 * Returns an array with the products' id and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return ProductListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of suppliers from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class SupplierList extends DataList{
+	/**
+	 * Returns an array with the suppliers' id and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return SupplierListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of user roles from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class RoleList extends DataList{
+	/**
+	 * Returns an array with the user roles' id and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return RoleListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of branches from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class BranchList extends DataList{
+	/**
+	 * Returns an array with the branches' id and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return BranchListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of payment card types from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class PaymentCardTypeList extends DataList{
+	/**
+	 * Returns an array with the payment card types' id and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return PaymentCardTypeListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of cash register shifts from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class ShiftList extends DataList{
+	/**
+	 * Returns an array with the cash register shifts' id, name and time_table from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return ShiftListDAM::getList($page, $total_pages, $total_items);
+	}
+}
+
+
+/**
+ * Utility class for obtaining a list of units of measure from the database.
+ * @package List
+ * @author Roberto Oliveros
+ */
+class UnitOfMeasureList extends DataList{
+	/**
+	 * Returns an array with the units' of measure id and name from the database.
+	 *
+	 * The total_pages and total_items parameters are necessary to return their respective values. If no page
+	 * argument is passed or a cero is passed, all the details are returned.
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getList($page = 0, &$total_pages = 0, &$total_items = 0){
+		if($page !== 0)
+			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
+			
+		return UnitOfMeasureListDAM::getList($page, $total_pages, $total_items);
+	}
+}
 ?>
