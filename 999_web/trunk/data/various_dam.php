93d4c5fb49ed7430f6ceabe8d3379713afbee5c9
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-05-19 23:47:48 +0000

    First commit and addition of the ClosingEventDAM class with dummy routine.

---
+++ 999_web/trunk/data/various_dam.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Library for accessing the database for multiple purposes.
+ * @package VariousDAM
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Class to perform a closing event in the system's data.
+ * @package Various
+ * @author Roberto Oliveros
+ */
+class ClosingEventDAM{
+	/**
+	 * Makes a closing on the system's data leaving only the provided months of information.
+	 *
+	 * @param integer $months
+	 */
+	static public function apply($months){
+		// Code here...
+	}
+}
+?>
