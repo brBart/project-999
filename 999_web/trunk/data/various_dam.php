34581b86aed2c7c5a9915bb05943c7f9b8a5cc42
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-05-22 00:08:30 +0000

    Addition of the DiscountReportDAM class with dummy routines.

--- 999_web/trunk/data/various_dam.php
+++ 999_web/trunk/data/various_dam.php
@@ -46,4 +46,65 @@ class CompanyDAM{
 		// Code here...
 	}
 }
+
+
+/**
+ * Utility class for accessing database information regarding the change of prices report.
+ * @package VariousDAM
+ * @author Roberto Oliveros
+ */
+class ChangePriceReportDAM{
+	/**
+	 * Retuns an array with the report information.
+	 *
+	 * The array's fields are date, username, bar_code, manufacturer, name, packaging, last_price and new_price.
+	 * If no page argument or cero is passed all the details are returned. The total_pages and total_items
+	 * arguments are necessary to return their respective values.
+	 * @param string $firstDate
+	 * @param string $lastDate
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getData($firstDate, $lastDate, $page, &$total_pages, &$total_items){
+		$total_pages = 1;
+		$total_items = 2;
+		return array(array('date' => '21/04/2009', 'username' => 'roboli', 'bar_code' => '32532',
+				'manufacturer' => 'Mattel', 'name' => 'barby', 'packaging' => 'caja', 'last_price' => 43.80,
+				'new_price' => 45.00), array('date' => '25/04/2009', 'username' => 'roboli',
+				'bar_code' => '92137', 'manufacturer' => 'Mattel', 'name' => 'joe', 'packaging' => 'caja',
+				'last_price' => 40.90, 'new_price' => 41.00));
+	}
+}
+
+
+/**
+ * Utility class for obtaing the discount report data from the database.
+ * @package VariousDAM
+ * @author Roberto Oliveros
+ */
+class DiscountReportDAM{
+	/**
+	 * Retuns an array with the report information.
+	 *
+	 * The array's fields are date, username, serial_number, number, subtotal, percentage, amount and total.
+	 * If no page argument or cero is passed all the details are returned. The total_pages and total_items
+	 * arguments are necessary to return their respective values. Date format: 'dd/mm/yyyy'.
+	 * @param string $firstDate
+	 * @param string $lastDate
+	 * @param integer $page
+	 * @param integer &$total_pages
+	 * @param integer &$total_items
+	 * @return array
+	 */
+	static public function getData($firstDate, $lastDate, $page, &$total_pages, &$total_items){
+		$total_pages = 1;
+		$total_items = 2;
+		return array(array('date' => '01/05/2009', 'username' => 'roboli', 'serial_number' => 'A021',
+				'number' => '4359', 'subtotal' => 124.83, 'percentage' => 12.00, 'total' => 100.83),
+				array('date' => '05/05/2009', 'username' => 'roboli', 'serial_number' => 'A021',
+				'number' => '1351', 'subtotal' => 245.83, 'percentage' => 12.00, 'total' => 200.83));
+	}
+}
 ?>
