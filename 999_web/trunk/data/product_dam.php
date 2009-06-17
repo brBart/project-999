800d9e015ef4a4e3f55906ccea8e394f929b196d
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-06-17 22:14:07 +0000

    Still modifying BonusDAM class.

--- 999_web/trunk/data/product_dam.php
+++ 999_web/trunk/data/product_dam.php
@@ -653,15 +653,17 @@ class BonusDAM{
 	}
 	
 	/**
-	 * Returns an instance of a bonus.
+	 * Returns an bonus' id which has the provided product and quantity.
 	 *
-	 * Returns the bonus which belongs to the provided product and contains the same quantity.
+	 * If not found returns 0.
 	 * @param Product $product
 	 * @param integer $quantity
-	 * @return Bonus
+	 * @return integer
 	 */
-	static public function getIdByProduct(Product $product, $quantity){
-		
+	static public function getId(Product $product, $quantity){
+		$sql = 'CALL bonus_id_get(:product_id, :quantity)';
+		$params = array(':product_id' => $product->getId(), ':quantity' => $quantity);
+		return (int)DatabaseHandler::getOne($sql, $params);
 	}
 	
 	/**
