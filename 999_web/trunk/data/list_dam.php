b49f3eaa721ff764fc1a44b8fc46e4691cffd3c6
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-06-22 22:46:03 +0000

    BankListDAM class modified.

--- 999_web/trunk/data/list_dam.php
+++ 999_web/trunk/data/list_dam.php
@@ -6,49 +6,71 @@
  */
 
 /**
- * Class for accesing database data for creating lists.
+ * For accessing the database.
+ */
+require_once('data/database_handler.php');
+
+/**
+ * Class for accesing database data for creating bank lists.
  * @package ListDAM
  * @author Roberto Oliveros
  */
 class BankListDAM{
 	/**
-	 * Returns an array with the banks' ids and names from the database.
+	 * Returns an array with the fields bank_id and name from the banks in the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
-		return array(array('id' => 123, 'name' => 'Gyt'), array('id' => 124, 'name' => 'Bi'));
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$sql = 'CALL bank_list_count()';
+		$totalItems = DatabaseHandler::getOne($sql);
+		
+		$totalPages = ceil($totalItems / ITEMS_PER_PAGE);
+		
+		if($page > 0)
+			$params = array(':start_item' => ($page - 1) * ITEMS_PER_PAGE, 'items_per_page' => ITEMS_PER_PAGE);
+		else
+			$params = array(':start_item' => 0, ':items_per_page' => $totalItems);
+		
+		$sql = 'CALL bank_list_get(:start_item, :items_per_page)';
+		return DatabaseHandler::getAll($sql, $params);
 	}
 }
 
 
 /**
- * Class for accesing database data for creating lists.
+ * Class for accesing database data for creating pending of confirmation deposit lists.
  * @package ListDAM
  * @author Roberto Oliveros
  */
 class PendingDepositListDAM{
 	/**
-	 * Returns an array with the deposits' date, id, number, bank_account, bank and amount from the database.
+	 * Returns an array with the deposits' created_date, deposit_id, number, bank_account, bank and total from
+	 * the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
-		return array(array('date' => '01/02/2009', 'id' => 123, 'number' => '4822', 'bank_acount' => '29500530',
-				'bank' => 'Gyt', 'amount' => 344.24), array('date' => '02/04/2009', 'id' => 124,
-				'number' => '7223', 'bank_acount' => '29500530', 'bank' => 'Bi', 'amount' => 145.10));
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$sql = 'CALL deposit_pending_list_count()';
+		$totalItems = DatabaseHandler::getOne($sql);
+		
+		$totalPages = ceil($totalItems / ITEMS_PER_PAGE);
+		
+		if($page > 0)
+			$params = array(':start_item' => ($page - 1) * ITEMS_PER_PAGE, 'items_per_page' => ITEMS_PER_PAGE);
+		else
+			$params = array(':start_item' => 0, ':items_per_page' => $totalItems);
+		
+		$sql = 'CALL deposit_pending_list_get(:start_item, :items_per_page)';
+		return DatabaseHandler::getAll($sql, $params);
 	}
 }
 
@@ -62,15 +84,15 @@ class ManufacturerListDAM{
 	/**
 	 * Returns an array with the manufacturers' id and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('id' => 123, 'name' => 'Bayer'), array('id' => 124, 'name' => 'Novartis'));
 	}
 }
@@ -85,15 +107,15 @@ class CorrelativeListDAM{
 	/**
 	 * Returns an array with the correlatives' serial_number and default flag from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('serial_number' => 'A021', 'default' => 1), array('serial_number' => 'A022',
 				'default' => 0));
 	}
@@ -109,15 +131,15 @@ class BankAccountListDAM{
 	/**
 	 * Returns an array with the bank accounts' number and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('number' => '2950000', 'name' => 'Central'), array('number' => '49230004',
 				'name' => 'Jutiapa'));
 	}
@@ -133,15 +155,15 @@ class UserAccountListDAM{
 	/**
 	 * Returns an array with the user accounts' username and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('username' => 'roboli', 'name' => 'Roberto'), array('username' => 'josoli',
 				'name' => 'Jose'));
 	}
@@ -157,15 +179,15 @@ class PaymentCardBrandListDAM{
 	/**
 	 * Returns an array with the payment card brands' id and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('id' => 123, 'name' => 'Visa'), array('id' => 124, 'name' => 'Master Card'));
 	}
 }
@@ -180,15 +202,15 @@ class ProductListDAM{
 	/**
 	 * Returns an array with the products' id and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('id' => 123, 'name' => 'Aspirina'), array('id' => 124, 'name' => 'Pepto Bismol'));
 	}
 }
@@ -203,15 +225,15 @@ class SupplierListDAM{
 	/**
 	 * Returns an array with the suppliers' id and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('id' => 123, 'name' => 'Central'), array('id' => 124, 'name' => 'Xela'));
 	}
 }
@@ -226,15 +248,15 @@ class RoleListDAM{
 	/**
 	 * Returns an array with the user roles' id and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('id' => 123, 'name' => 'Administrador'), array('id' => 124, 'name' => 'Operador'));
 	}
 }
@@ -249,15 +271,15 @@ class BranchListDAM{
 	/**
 	 * Returns an array with the branches' id and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('id' => 123, 'name' => 'Xela'), array('id' => 124, 'name' => 'Barberena'));
 	}
 }
@@ -272,15 +294,15 @@ class PaymentCardTypeListDAM{
 	/**
 	 * Returns an array with the payment card types' id and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('id' => 123, 'name' => 'Credito'), array('id' => 124, 'name' => 'Debito'));
 	}
 }
@@ -295,15 +317,15 @@ class ShiftListDAM{
 	/**
 	 * Returns an array with the cash register shifts' id, name and time_table from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('id' => 123, 'name' => 'Diurno', 'time_table' => '8am - 12pm'), array('id' => 124,
 				'name' => 'Nocturno', 'time_table' => '6pm - 11pm'));
 	}
@@ -319,15 +341,15 @@ class UnitOfMeasureListDAM{
 	/**
 	 * Returns an array with the units' of measure id and name from the database.
 	 *
-	 * The total_pages and total_items parameters are necessary to return their respective values.
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * The totalPages and totalItems parameters are necessary to return their respective values.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList(&$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
+	static public function getList(&$totalPages, &$totalItems, $page){
+		$totalPages = 1;
+		$totalItems = 2;
 		return array(array('id' => 123, 'name' => 'Unidad'), array('id' => 124, 'name' => 'Docena'));
 	}
 }
