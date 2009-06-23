f20d1d1533561d63f7998ce6c6129b95b9f370b3
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-06-23 19:24:45 +0000

    UnitOfMeasureListDAM class modified.

--- 999_web/trunk/data/list_dam.php
+++ 999_web/trunk/data/list_dam.php
@@ -429,13 +429,13 @@ class ShiftListDAM{
 
 
 /**
- * Class for accesing database data for creating lists.
+ * Class for accesing database data for creating unit of measure lists.
  * @package ListDAM
  * @author Roberto Oliveros
  */
 class UnitOfMeasureListDAM{
 	/**
-	 * Returns an array with the units' of measure id and name from the database.
+	 * Returns an array with the fields unit_measure_id and name from all the units of measure in the database.
 	 *
 	 * The totalPages and totalItems parameters are necessary to return their respective values.
 	 * @param integer &$totalPages
@@ -444,9 +444,18 @@ class UnitOfMeasureListDAM{
 	 * @return array
 	 */
 	static public function getList(&$totalPages, &$totalItems, $page){
-		$totalPages = 1;
-		$totalItems = 2;
-		return array(array('id' => 123, 'name' => 'Unidad'), array('id' => 124, 'name' => 'Docena'));
+		$sql = 'CALL unit_of_measure_list_count()';
+		$totalItems = DatabaseHandler::getOne($sql);
+		
+		$totalPages = ceil($totalItems / ITEMS_PER_PAGE);
+		
+		if($page > 0)
+			$params = array(':start_item' => ($page - 1) * ITEMS_PER_PAGE, 'items_per_page' => ITEMS_PER_PAGE);
+		else
+			$params = array(':start_item' => 0, ':items_per_page' => $totalItems);
+		
+		$sql = 'CALL unit_of_measure_list_get(:start_item, :items_per_page)';
+		return DatabaseHandler::getAll($sql, $params);
 	}
 }
 ?>
