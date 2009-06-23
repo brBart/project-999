025820b0a89f92eb6c5187ace19aa06249c1c51c
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-06-23 17:46:11 +0000

    ProductListDAM class modified.

--- 999_web/trunk/data/list_dam.php
+++ 999_web/trunk/data/list_dam.php
@@ -237,13 +237,13 @@ class PaymentCardBrandListDAM{
 
 
 /**
- * Class for accesing database data for creating lists.
+ * Class for accesing database data for creating product lists.
  * @package ListDAM
  * @author Roberto Oliveros
  */
 class ProductListDAM{
 	/**
-	 * Returns an array with the products' id and name from the database.
+	 * Returns an array with the fields product_id, name and packaging from all the products in the database.
 	 *
 	 * The totalPages and totalItems parameters are necessary to return their respective values.
 	 * @param integer &$totalPages
@@ -252,9 +252,18 @@ class ProductListDAM{
 	 * @return array
 	 */
 	static public function getList(&$totalPages, &$totalItems, $page){
-		$totalPages = 1;
-		$totalItems = 2;
-		return array(array('id' => 123, 'name' => 'Aspirina'), array('id' => 124, 'name' => 'Pepto Bismol'));
+		$sql = 'CALL product_list_count()';
+		$totalItems = DatabaseHandler::getOne($sql);
+		
+		$totalPages = ceil($totalItems / ITEMS_PER_PAGE);
+		
+		if($page > 0)
+			$params = array(':start_item' => ($page - 1) * ITEMS_PER_PAGE, 'items_per_page' => ITEMS_PER_PAGE);
+		else
+			$params = array(':start_item' => 0, ':items_per_page' => $totalItems);
+		
+		$sql = 'CALL product_list_get(:start_item, :items_per_page)';
+		return DatabaseHandler::getAll($sql, $params);
 	}
 }
 
