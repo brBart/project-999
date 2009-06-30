6405d0acaeff93a673ccfcd91f6754aa4e237325
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-06-30 01:17:47 +0000

    DiscountListDAM class modified.

--- 999_web/trunk/data/various_dam.php
+++ 999_web/trunk/data/various_dam.php
@@ -102,25 +102,33 @@ class ChangePriceListDAM{
  */
 class DiscountListDAM{
 	/**
-	 * Retuns an array with the report information.
+	 * Retuns an array with data regarding invoice sales discounts that ocurred between the provided dates.
 	 *
-	 * The array's fields are date, username, serial_number, number, subtotal, percentage, amount and total.
-	 * If no page argument or cero is passed all the details are returned. The total_pages and total_items
-	 * arguments are necessary to return their respective values. Date format: 'dd/mm/yyyy'.
+	 * The array's fields are create_date, user_account_username, serial_number, number, subtotal, percentage,
+	 * amount and total. If no page argument or cero is passed all the details are returned. The totalPages and
+	 * totalItems arguments are necessary to return their respective values. Date format: 'dd/mm/yyyy'.
 	 * @param string $firstDate
 	 * @param string $lastDate
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function getList($firstDate, $lastDate, &$total_pages, &$total_items, $page){
-		$total_pages = 1;
-		$total_items = 2;
-		return array(array('date' => '01/05/2009', 'username' => 'roboli', 'serial_number' => 'A021',
-				'number' => '4359', 'subtotal' => 124.83, 'percentage' => 12.00, 'total' => 100.83),
-				array('date' => '05/05/2009', 'username' => 'roboli', 'serial_number' => 'A021',
-				'number' => '1351', 'subtotal' => 245.83, 'percentage' => 12.00, 'total' => 200.83));
+	static public function getList($firstDate, $lastDate, &$totalPages, &$totalItems, $page){
+		$sql = 'CALL discount_list_count(:first_date, :last_date)';
+		$params = array(':first_date' => Date::dbFormat($firstDate), ':last_date' => Date::dbFormat($lastDate));
+		$totalItems = DatabaseHandler::getOne($sql, $params);
+		
+		$totalPages = ceil($totalItems / ITEMS_PER_PAGE);
+		
+		if($page > 0)
+			$params = array_merge($params, 
+					array(':start_item' => ($page - 1) * ITEMS_PER_PAGE, 'items_per_page' => ITEMS_PER_PAGE));
+		else
+			$params = array_merge($params, array(':start_item' => 0, ':items_per_page' => $totalItems));
+		
+		$sql = 'CALL discount_list_get(:first_date, :last_date, :start_item, :items_per_page)';
+		return DatabaseHandler::getAll($sql, $params);
 	}
 }
 ?>
