130f34a5496dbfdde08658b84bf56541af5dd3e1
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-07-01 15:05:37 +0000

    ClosingEventDAM documentation fixes.

--- 999_web/branches/Busines_Finished_Stable/data/various_dam.php
+++ 999_web/branches/Busines_Finished_Stable/data/various_dam.php
@@ -12,11 +12,11 @@
  */
 class ClosingEventDAM{
 	/**
-	 * Makes a closing on the system's data leaving only the provided months of information.
+	 * Makes a closing on the system's data leaving only the provided days of information.
 	 *
-	 * @param integer $months
+	 * @param integer $days
 	 */
-	static public function apply($months){
+	static public function apply($days){
 		// Code here...
 	}
 }
