a87899613dc4c05825a21d870f8c9c08ecf0dac0
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-07-11 00:17:42 +0000

    Renamed to login_operations.php

---
+++ 999_pos/trunk/commands/login_operations.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Library containing the ShowLoginOperations command.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+
+/**
+ * Displays the login form for the operations side of the system.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class LoginOperationsCommand extends Command{
+	/**
+	 * (non-PHPdoc)
+	 * @see 999_web/presentation/commands/Command#execute($request, $helper)
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		Page::display(array('success' => '1'), 'login_form_operations_html.tpl');
+	}
+}
+?>
