775ca8b18d4ccfd112e2bde10f5910429f776319
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-07-13 22:08:33 +0000

    First commit.

---
+++ 999_pos/trunk/commands/show_not_found_operations.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Library containing the ShowNotFoundOperations command.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+
+/**
+ * Command to display the not found message on the operations site.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class ShowNotFoundOperationsCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$msg = 'Comando no existe.';
+		Page::display(array('main_menu' => 'main_menu_operations.tpl', 'second_menu' => 'blank.tpl',
+				'content' => 'blank.tpl', 'success' => '0', 'message' => $msg), 'operations_html.tpl');
+	}
+}
+?>
