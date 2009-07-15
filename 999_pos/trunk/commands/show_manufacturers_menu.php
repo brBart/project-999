d4bfdc01dc73c66f272cfd7727f171f6dc326ca4
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-07-15 00:45:12 +0000

    First commit.

---
+++ 999_pos/trunk/commands/show_manufacturers_menu.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Library containing the ShowManufacturersMenu command.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+
+/**
+ * Command to display the manufacturers menu.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class ShowManufacturersMenuCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$back_trace = array('Inicio', 'Mantenimiento', 'Casas');
+		Page::display(array('module_title' => OPERATIONS_TITLE, 'main_menu' => 'main_menu_operations_html.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'maintenance_menu_operations_html.tpl',
+				'content' => 'manufacturers_menu_html.tpl', 'notify' => '0'), 'site_html.tpl');
+	}
+}
+?>
---
+++ 999_pos/trunk/templates/manufacturers_menu_html.tpl
@@ -0,0 +1,5 @@
+{* Smarty * }
+<ul>
+    <li><a href="index.php?cmd=create_manufacturer">Crear</a></li>
+    <li><a href="index.php?cmd=get_manufacturer">Consultar</a></li>
+</ul>
