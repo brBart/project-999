84de51b63163d577362575d66bca8dc3f1f19cd5
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-07-15 00:07:22 +0000

    First commit.

---
+++ 999_pos/trunk/commands/show_maintenance_menu_operations.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Library containing the ShowMaintenanceMenuOperations command.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+
+/**
+ * Command to display the maintenance menu on the operations site.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class ShowMaintenanceMenuOperationsCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$back_trace = array('Inicio', 'Mantenimiento');
+		Page::display(array('module_title' => OPERATIONS_TITLE, 'main_menu' => 'main_menu_operations_html.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'maintenance_menu_operations_html.tpl',
+				'content' => 'blank.tpl', 'notify' => '0'), 'site_html.tpl');
+	}
+}
+?>
---
+++ 999_pos/trunk/templates/maintenance_menu_operations_html.tpl
@@ -0,0 +1,9 @@
+{* Smarty * }
+<ul>
+    <li><a href="index.php?cmd=show_manufacturers_menu">Casas</a></li>
+    <li><a href="index.php?cmd=show_units_of_measure_menu">Unidades de Medida</a></li>
+    <li><a href="index.php?cmd=show_products_menu">Productos</a></li>
+    <li><a href="index.php?cmd=show_bonus_menu">Ofertas</a></li>
+    <li><a href="index.php?cmd=show_suppliers_menu">Proveedores</a></li>
+    <li><a href="index.php?cmd=show_branchs_menu">Sucursales</a></li>
+</ul>
