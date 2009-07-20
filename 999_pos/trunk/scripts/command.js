7520df8656909434525bc9d5a62e3023f7488614
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-07-20 16:50:40 +0000

    The createXmlHttpRequest method was removed from the object to avoid unnecessary callings.

--- 999_pos/trunk/scripts/command.js
+++ 999_pos/trunk/scripts/command.js
@@ -8,7 +8,7 @@
  * Constructor function for creating the request object and setting the console for displaying the results.
  * @param Console oConsole
  */
-function Command(oConsole){
+function Command(oConsole, oRequest){
 	/**
 	  * Holds the console where to display the results.
 	  * @var Console
@@ -19,47 +19,7 @@ function Command(oConsole){
 	 * Holds the request object.
 	 * @var XmlHttpRequest
 	 */
-	 this.request = this.createXmlHttpRequestObject();
-}
-
-/**
- * Creates the adequate object depending on the browser.
- * @return XmlHttpRequest 
- */
-Command.prototype.createXmlHttpRequestObject = function(){
-	// will store the reference to the XMLHttpRequest object
-	var xmlHttp;
-	// this should work for all browsers except IE6 and older
-	try
-		{
-	    	// try to create XMLHttpRequest object
-	    	xmlHttp = new XMLHttpRequest();
-		}
-	  	catch(e)
-	  	{
-	  		// assume IE6 or older
-	  		var XmlHttpVersions = new Array("MSXML2.XMLHTTP.6.0",
-	  					"MSXML2.XMLHTTP.5.0",
-	                    "MSXML2.XMLHTTP.4.0",
-	                    "MSXML2.XMLHTTP.3.0",
-	                    "MSXML2.XMLHTTP",
-	                    "Microsoft.XMLHTTP");
-	  		// try every id until one works
-	  		for (var i=0; i<XmlHttpVersions.length && !xmlHttp; i++) 
-	  		{
-	  			try 
-	  			{ 
-	  				// try to create XMLHttpRequest object
-	  				xmlHttp = new ActiveXObject(XmlHttpVersions[i]);
-	  			} 
-	  			catch (e) {} // ignore potential error
-	  		}
-	  	}
-	  	// return the created object or display an error message
-	  	if (!xmlHttp)
-	  		this.console.displayError('Interno: Imposible crear el objeto XmlHttpRequest.');
-	  	else 
-	  		return xmlHttp;
+	 this.request = oRequest;
 }
 
 /**
