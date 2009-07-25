52cfdbccdba1baa8b62f1d3d6cb757eca7d1f9ec
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-07-25 00:46:42 +0000

    First commit.

---
+++ 999_pos/trunk/scripts/state_machine.js
@@ -0,0 +1,37 @@
+/**
+ * Library with the state machine class in charge of controlling the state of each element in the form.
+ * @package Client
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Constructor. Does nothing because methods are static.
+ */
+function StateMachine(){}
+
+/**
+ * Set the focus on the element with the provided id.
+ * @param string sElementId
+ */
+StateMachine.setFocus = function(sElementId){
+	var oElement = document.getElementById(sElementId);
+	oElement.focus();
+}
+
+/**
+ * Set the form the edit state.
+ */
+StateMachine.changeToEditState = function(){
+	// Disabled and enabled the required widgets in the form.
+	var arrElements = document.getElementsByName('form_widget');
+	for (var oElement in arrElements){
+		if(oElement.disabled == true)
+			oElement.disabled == false;
+		else
+			oElement.disabled == true;
+	}
+	
+	// Change the status_label to edit.
+	var oStatus = document.getElementById('status_label');
+	oStatus.innerHTML = 'Editando...';
+}
