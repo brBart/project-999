64c7f64b14fb0ad590363ea0d7e26583e7acd275
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-07-29 21:58:48 +0000

    One error at a time. Reset method added.

--- 999_pos/trunk/scripts/console.js
+++ 999_pos/trunk/scripts/console.js
@@ -38,7 +38,12 @@ Console.prototype.displayFailure = function(sMsg, sElementId){
 * @param string sMsg
 */
 Console.prototype.displayError = function(sMsg){
-	var newP = '<p class="error">' + sMsg + '</p>';
+	var elementP = document.getElementById('error');
+	// If there was a message.
+	if(elementP)
+		this._mElementDiv.removeChild(elementP);
+	
+	var newP = '<p id="error" class="error">' + sMsg + '</p>';
 	// Display the error message.
 	this.displayMessage(newP);
 }
@@ -50,7 +55,7 @@ Console.prototype.displayError = function(sMsg){
 Console.prototype.cleanFailure = function(sElementId){
 	var elementP = document.getElementById('failed_' + sElementId);
 	
-	// If there was no message.
+	// If there was a message.
 	if(elementP){
 		this._mElementDiv.removeChild(elementP);
 		this._mElementDiv.scrollTop = this._mElementDiv.scrollHeight;
@@ -58,4 +63,18 @@ Console.prototype.cleanFailure = function(sElementId){
 		var oElement = document.getElementById(sElementId + '_failed');
 		oElement.className = 'hidden';
 	}
+}
+ 
+/**
+ * Clean the element div from all messages. Elements fields as well.
+ */
+Console.prototype.reset = function(){
+	var arrElements = this._mElementDiv.getElementsByTagName('*');
+	for (var i = 0; i < arrElements.length; i++){
+		var sId = arrElements[i].getAttribute('id'); 
+		if(sId != 'error')
+			this.cleanFailure(sId.substring(sId.indexOf('_') + 1));
+	}
+	
+	this._mElementDiv.innerHTML = '';
 }
