0e392dab44a0faf5c3ae6332775e4f8ff2f858af
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-07-29 14:17:54 +0000

    For in removed for traditional for and other improvements.

--- 999_pos/trunk/scripts/state_machine.js
+++ 999_pos/trunk/scripts/state_machine.js
@@ -21,17 +21,20 @@ StateMachine.setFocus = function(sElementId){
 /**
  * Set the form the edit state.
  */
-StateMachine.changeToEditState = function(){
+StateMachine.changeToEditState = function(sElementId){
 	// Disabled and enabled the required widgets in the form.
 	var arrElements = document.getElementsByName('form_widget');
-	for (var oElement in arrElements){
+	for (var i = 0; i < arrElements.length; i++){
+		var oElement = arrElements[i];
 		if(oElement.disabled == true)
-			oElement.disabled == false;
+			oElement.disabled = false;
 		else
-			oElement.disabled == true;
+			oElement.disabled = true;
 	}
 	
 	// Change the status_label to edit.
 	var oStatus = document.getElementById('status_label');
 	oStatus.innerHTML = 'Editando...';
+	
+	this.setFocus(sElementId);
 }
