c05eaf513fc5328309a991eec7db9aae2c0255e3
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-08-01 01:42:49 +0000

    Renamed to sync.

---
+++ 999_pos/trunk/scripts/sync.js
@@ -0,0 +1,34 @@
+/**
+ * Library with the Async command base class.
+ * @package Client
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Constructor function.
+ * @param Session oSession
+ * @param Console oConsole
+ * @param Request oRequest
+ */
+function AsyncCommand(oSession, oConsole, oRequest){
+	// Call the parent constructor.
+	Command.call(this, oSession, oConsole, oRequest);
+}
+
+/**
+* Inherit the Command class methods.
+*/
+AsyncCommand.prototype = new Command();
+
+/**
+ * Send the request to the server.
+ * @param string sUrlParams
+ */
+AsyncCommand.prototype.sendRequest = function(sUrlParams){
+	sUrlParams = Url.addUrlParam(sUrlParams, 'type', 'xml');
+	this._mRequest.open('GET', sUrlParams, false);
+	this._mRequest.send(null);
+	this.handleRequestStateChange();
+}
+
+
