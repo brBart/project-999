c65248afcfa75f66ae79c70135eae3124b63f769
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-08-06 23:59:53 +0000

    First commit.

---
+++ 999_pos/trunk/commands/set_packaging_product.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Library containing the SetPackagingProduct class command.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('commands/set_property_object.php');
+
+/**
+ * Defines functionality for setting the product's packaging.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class SetPackagingProductCommand extends SetPropertyObjectCommand{
+	/**
+	 * Set the desired property on the object.
+	 * @param variant $value
+	 * @param variant $obj
+	 */
+	protected function setProperty($value, $obj){
+		$obj->setPackaging($value);
+	}
+}
+?>
