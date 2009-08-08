0f24ae56f597796b8c20600a43d88ec3adcb0bfb
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-08-08 19:37:23 +0000

    Still working on it.

--- 999_pos/trunk/commands/add_supplier_product.php
+++ 999_pos/trunk/commands/add_supplier_product.php
@@ -13,6 +13,10 @@ require_once('presentation/command.php');
  * For displaying the results.
  */
 require_once('presentation/page.php');
+/**
+ * For accessing the supplier data.
+ */
+require_once('business/agent.php');
 
 /**
  * Defines functionality for adding a supplier to a product.
@@ -26,7 +30,38 @@ abstract class AddSupplierProductCommand extends Command{
 	 * @param SessionHelper $helper
 	 */
 	public function execute(Request $request, SessionHelper $helper){
-		$id = $request->getProperty('supplierid');
+		$id = $request->getProperty('supplier_id');
+		// Check if the user selected a supplier.
+		if($id == ''){
+			$msg = 'Seleccione un proveedor.';
+			Page::display(array('success' => '0', 'element_id' => 'supplier_id', 'message' => $msg),
+					'validate_xml.tpl');
+			return;
+		}
+		
+		$supplier = Supplier::getInstance((int)$id);
+		// If id is not valid.
+		if(is_null($supplier)){
+			$msg = 'Proveedor no existe.';
+			Page::display(array('message' => $msg), 'error_xml.tpl');
+		}
+		else{
+			$product_sku = $request->getProperty('product_sku');
+			
+			try{
+				$detail = new ProductSupplier($supplier, $product_sku);
+			} catch(ValidateException $e){
+				$msg = $e->getMessage();
+				$element_id = $e->getProperty();
+				Page::display(array('success' => '0', 'element_id' => $element_id, 'message' => $msg),
+						'validate_xml.tpl');
+				return;
+			} catch(Exception $e){
+				$msg = $e->getMessage();
+				Page::display(array('message' => $msg), 'error_xml.tpl');
+				return;
+			}
+		}
 	}
 }
 ?>
