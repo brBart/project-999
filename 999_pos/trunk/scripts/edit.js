59dfe636aecc32222e057bab2def5d9cc7e61d1f
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-08-16 00:45:31 +0000

    Modifications made because state machine became an object.

--- 999_pos/trunk/scripts/edit.js
+++ 999_pos/trunk/scripts/edit.js
@@ -9,13 +9,21 @@
  * @param Session oSession
  * @param Console oConsole
  * @param Request oRequest
+ * @param StateMachine oMachine
  */
-function EditCommand(oSession, oConsole, oRequest){
+function EditCommand(oSession, oConsole, oRequest, oMachine){
 	// Call the parent constructor.
 	SyncCommand.call(this, oSession, oConsole, oRequest);
 	
 	/**
+	 * Holds the form state machine.
+	 * @var StateMachine
+	 */
+	this._mMachine = oMachine;
+	
+	/**
 	 * Holds the id of the element to receive focus in case on success.
+	 * @var string
 	 */
 	this._mElementId = '';
 }
@@ -45,5 +53,5 @@ EditCommand.prototype.execute = function(sCmd, sElementId){
 * @param DocumentElement xmlDoc
 */
 EditCommand.prototype.displaySuccess = function(xmlDoc){
-	StateMachine.changeToEditState(this._mElementId);
+	this._mMachine.changeToEditState(this._mElementId);
 }
