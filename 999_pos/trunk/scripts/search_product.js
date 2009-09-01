f1e519467e02054e9b929126d2b6a60f17c73c1b
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-09-01 15:46:03 +0000

    Formatting file.

--- 999_pos/trunk/scripts/search_product.js
+++ 999_pos/trunk/scripts/search_product.js
@@ -156,13 +156,13 @@ SearchProduct.prototype.checkForChanges  = function(){
 	// retrieve the keyword object 
 	var keyword = this._mTxtWidget.value;
 	// check to see if the keyword is empty
-	if(keyword == "")
+	if(keyword == '')
 	{
 	  // hide the suggestions
 	  this.hideSuggestions();
 	  // reset the keywords 
-	  this._mUserKeyword="";
-	  this._mHttpRequestKeyword="";
+	  this._mUserKeyword = '';
+	  this._mHttpRequestKeyword = '';
 	}
 	// set the timer for a new check 
 	oTemp = this;
@@ -174,7 +174,57 @@ SearchProduct.prototype.checkForChanges  = function(){
 	  // update the suggestions
 	  this.getSuggestions(keyword);
 }
+  
+/**
+ * Method that checks to see if the keyword specified as parameter is in the cache or tries to find the
+ * longest matching prefixes in the cache and adds them in the cache for the current keyword parameter.
+ * @param string sKeyword
+ */
+SearchProduct.prototype.checkCache = function(sKeyword){
+	// check to see if the sKeyword is already in the cache
+	if(this._mCache[sKeyword])
+		return true;
+	// try to find the biggest prefixes
+	for(i = sKeyword.length-2; i >= 0; i--)
+	{
+		// compute the current prefix sKeyword 
+		var currentKeyword = sKeyword.substring(0, i + 1);
+ 		// check to see if we have the current prefix sKeyword in the cache
+ 		if(this._mCache[currentKeyword])
+ 		{            
+ 			// the current keyword's results already in the cache
+ 			var cacheResults = this._mCache[currentKeyword];
+ 			// the results matching the sKeyword in the current cache results
+ 			var keywordResults = new Array();
+ 			var keywordResultsSize = 0;            
+ 			// try to find all matching results starting with the current prefix
+ 			for(j = 0; j < cacheResults.length; j++)
+ 			{
+ 				if(cacheResults[j].name.indexOf(sKeyword) == 0)               
+ 					keywordResults[keywordResultsSize++] = cacheResults[j];
+ 			}      
+ 			// add all the sKeyword's prefix results to the cache
+ 			this.addToCache(sKeyword, keywordResults);      
+ 			return true;  
+ 		}
+ 	}
+ 	// no match found
+ 	return false;
+}
 
+/**
+ * Method that adds to a keyword an array of values.
+ * @param string sKeyword
+ * @param array values
+ */
+SearchProduct.prototype.addToCache = function(sKeyword, values){
+	// create a new array entry in the cache
+	this._mCache[sKeyword] = new Array();
+ 	// add all the values to the sKeyword's entry in the cache
+ 	for(i = 0; i < values.length; i++)
+ 		this._mCache[sKeyword][i] = values[i];
+}
+ 
 /**
  * Hide the div containing the suggestions.
  */
@@ -251,53 +301,32 @@ SearchProduct.prototype.sendRequest = function(sUrlParams){
 }
 
 /**
- * Method that checks to see if the keyword specified as parameter is in the cache or tries to find the
- * longest matching prefixes in the cache and adds them in the cache for the current keyword parameter.
- * @param string sKeyword
- */
-SearchProduct.prototype.checkCache = function(sKeyword){
-	// check to see if the sKeyword is already in the cache
-	if(this._mCache[sKeyword])
-		return true;
-	// try to find the biggest prefixes
-	for(i=sKeyword.length-2; i>=0; i--)
+* Method for displaying success.
+* @param DocumentElement xmlDoc
+*/
+SearchProduct.prototype.displaySuccess = function(xmlDoc){
+	// initialize the new array of functions' names
+	nameArray = new Array();
+	// check to see if we have any results for the searched keyword
+ 
+	if(xmlDoc.childNodes.length)
 	{
-		// compute the current prefix sKeyword 
-		var currentKeyword = sKeyword.substring(0, i+1);
-		// check to see if we have the current prefix sKeyword in the cache
-		if(this._mCache[currentKeyword])
-		{            
-			// the current keyword's results already in the cache
-			var cacheResults = this._mCache[currentKeyword];
-			// the results matching the sKeyword in the current cache results
-			var keywordResults = new Array();
-			var keywordResultsSize = 0;            
-			// try to find all matching results starting with the current prefix
-			for(j=0;j<cacheResults.length;j++)
-			{
-				if(cacheResults[j].name.indexOf(sKeyword) == 0)               
-					keywordResults[keywordResultsSize++] = cacheResults[j];
-			}      
-			// add all the sKeyword's prefix results to the cache
-			this.addToCache(sKeyword, keywordResults);      
-			return true;  
-		}
+		/* we retrieve the new functions' names from the document element as 
+       	an array */
+		nameArray = this.xmlToArray(xmlDoc.getElementsByTagName('result'));       
 	}
-	// no match found
-	return false;
-}
-
-/**
- * Method that adds to a keyword an array of values.
- * @param string sKeyword
- * @param array values
- */
-SearchProduct.prototype.addToCache = function(sKeyword, values){
-	// create a new array entry in the cache
-	this._mCache[sKeyword] = new Array();
-	// add all the values to the sKeyword's entry in the cache
-	for(i=0; i<values.length; i++)
-		this._mCache[sKeyword][i] = values[i];
+	// check to see if other keywords are already being searched for
+	if(this._mHttpRequestKeyword == this._mUserKeyword)    
+	{
+		// display the results array
+		this.displayResults(this._mHttpRequestKeyword, nameArray);
+	}
+	else
+	{
+		// add the results to the cache
+		// we don't need to display the results since they are no longer useful
+		this.addToCache(this._mHttpRequestKeyword, nameArray);              
+	}
 }
 
 /**
@@ -307,15 +336,14 @@ SearchProduct.prototype.addToCache = function(sKeyword, values){
  */
 SearchProduct.prototype.displayResults = function(sKeyword, resultsArray){
 	// start building the HTML table containing the results
-	var div = "<table>"; 
+	var div = '<table>'; 
 	// if the searched for sKeyword is not in the cache then add it to the cache
 	if(!this._mCache[sKeyword] && sKeyword)
 		this.addToCache(sKeyword, resultsArray);
 	// if the array of results is empty display a message
 	if(resultsArray.length == 0)
 	{
-		div += "<tr><td>No results found for <strong>" + sKeyword + 
-        		"</strong></td></tr>";
+		div += '<tr><td>No resultados para <strong>' + sKeyword + '</strong></td></tr>';
 		// set the flag indicating that no results have been found 
 		// and reset the counter for results
 		this._mHasResults = false;
@@ -334,7 +362,7 @@ SearchProduct.prototype.displayResults = function(sKeyword, resultsArray){
 		// get the number of results from the cache
 		this._mSuggestions = this._mCache[sKeyword].length;
 		// loop through all the results and generate the HTML list of results
-		for (var i=0; i<this._mCache[sKeyword].length; i++) 
+		for (var i = 0; i < this._mCache[sKeyword].length; i++) 
 		{  
 			// retrieve the product id
 			crtProductBarCode = this._mCache[sKeyword][i].bar_code;
@@ -366,7 +394,7 @@ SearchProduct.prototype.displayResults = function(sKeyword, resultsArray){
 				else
 				{
 					// bold the entire function name
-					div += '<b>' + crtProductName.substring(0,this._mSuggestionMaxLength) + '</b></td>';          
+					div += '<b>' + crtProductName.substring(0, this._mSuggestionMaxLength) + '</b></td>';          
 				}
 			}
 			
@@ -377,15 +405,33 @@ SearchProduct.prototype.displayResults = function(sKeyword, resultsArray){
 	}
 	// end building the HTML table
 	div += '</table>';
-	var oSuggest = document.getElementById("suggest");
+	var oSuggest = document.getElementById('suggest');
 	// scroll to the top of the list
 	this._mScrollDiv.scrollTop = 0;
 	// update the suggestions list and make it visible
 	oSuggest.innerHTML = div;
-	this._mScrollDiv.style.visibility = "visible";
+	this._mScrollDiv.style.visibility = 'visible';
 	// if we had results we apply the type ahead for the current sKeyword
 	if(resultsArray.length > 0)
 		this.autocompleteKeyword();
+}
+ 
+/**
+ * Transforms all the children of an xml node into an array.
+ * @param Xml resultsXml
+ */
+SearchProduct.prototype.xmlToArray = function(resultsXml){
+ 	// initiate the resultsArray
+ 	var resultsArray = new Array();  
+ 	// loop through all the xml nodes retrieving the content
+ 	for(i = 0; i < resultsXml.length; i++){
+ 		resultsArray[i] = {};
+ 		resultsArray[i].bar_code = resultsXml.item(i).getElementsByTagName('bar_code')[0].firstChild.data;
+ 		resultsArray[i].name = resultsXml.item(i).getElementsByTagName('name')[0].firstChild.data;
+ 		resultsArray[i].packaging = resultsXml.item(i).getElementsByTagName('packaging')[0].firstChild.data;
+ 	}
+ 	// return the node's content as an array
+ 	return resultsArray;
 }
 
 /**
@@ -397,23 +443,23 @@ SearchProduct.prototype.autocompleteKeyword = function(){
 	// deselect all suggestions
 	this.deselectAll();
 	// highlight the selected suggestion 
-	document.getElementById("tr0").className="highlightrow";  
+	document.getElementById('tr0').className = 'highlightrow';  
 	// update the keyword's value with the suggestion
-	this.updateKeywordValue(document.getElementById("tr0"));  
+	this.updateKeywordValue(document.getElementById('tr0'));  
 	// apply the type-ahead style
-	this.selectRange(this._mTxtWidget,this._mHttpRequestKeyword.length,this._mTxtWidget.value.length);  
+	this.selectRange(this._mTxtWidget, this._mHttpRequestKeyword.length, this._mTxtWidget.value.length);  
 	// set the autocompleted word to the keyword's value
-	this._mAutocompletedKeyword=this._mTxtWidget.value;
+	this._mAutocompletedKeyword = this._mTxtWidget.value;
 }
 
 /**
  * Method that removes the style from all suggestions.
  */
 SearchProduct.prototype.selectAll = function(){
-	for(i=0; i<this._mSuggestions; i++)
+	for(i = 0; i < this._mSuggestions; i++)
 	{
-		var oCrtTr = document.getElementById("tr" + i);
-	    oCrtTr.className = "";    
+		var oCrtTr = document.getElementById('tr' + i);
+	    oCrtTr.className = '';    
 	}
 }
  
@@ -422,10 +468,10 @@ SearchProduct.prototype.selectAll = function(){
  */
 SearchProduct.prototype.deselectAll = function()
 { 
-	for(i=0; i<this._mSuggestions; i++)
+	for(i = 0; i < this._mSuggestions; i++)
 	{
-		var oCrtTr = document.getElementById("tr" + i);
-		oCrtTr.className = "";    
+		var oCrtTr = document.getElementById('tr' + i);
+		oCrtTr.className = '';    
 	}
 }
 
@@ -453,8 +499,8 @@ SearchProduct.prototype.selectRange = function(oText, start, length){
 	{
 		//IE
 		var oRange = oText.createTextRange(); 
-		oRange.moveStart("character", start); 
-		oRange.moveEnd("character", length - oText.value.length); 
+		oRange.moveStart('character', start); 
+		oRange.moveEnd('character', length - oText.value.length); 
 		oRange.select();
 	}
 	else 
@@ -473,7 +519,7 @@ SearchProduct.prototype.selectRange = function(oText, start, length){
 SearchProduct.prototype.handleOnMouseOver = function(oTr)
 {
 	this.deselectAll();
-	oTr.className = "highlightrow";  
+	oTr.className = 'highlightrow';  
 	this._mPosition = oTr.id.substring(2, oTr.id.length);
 }
 
@@ -483,55 +529,8 @@ SearchProduct.prototype.handleOnMouseOver = function(oTr)
  */
 SearchProduct.prototype.handleOnMouseOut = function(oTr)
 {
-	oTr.className = "";   
+	oTr.className = '';   
 	this._mPosition = -1;
-}
-
-/**
-* Method for displaying success.
-* @param DocumentElement xmlDoc
-*/
-SearchProduct.prototype.displaySuccess = function(xmlDoc){
-	// initialize the new array of functions' names
-	nameArray = new Array();
-	// check to see if we have any results for the searched keyword
- 
-	if(xmlDoc.childNodes.length)
-	{
-		/* we retrieve the new functions' names from the document element as 
-       	an array */
-		nameArray= this.xmlToArray(xmlDoc.getElementsByTagName('result'));       
-	}
-	// check to see if other keywords are already being searched for
-	if(this._mHttpRequestKeyword == this._mUserKeyword)    
-	{
-		// display the results array
-		this.displayResults(this._mHttpRequestKeyword, nameArray);
-	}
-	else
-	{
-		// add the results to the cache
-		// we don't need to display the results since they are no longer useful
-		this.addToCache(this._mHttpRequestKeyword, nameArray);              
-	}
-}
-
-/**
- * Transforms all the children of an xml node into an array.
- * @param Xml resultsXml
- */
-SearchProduct.prototype.xmlToArray = function(resultsXml){
-	// initiate the resultsArray
-	var resultsArray = new Array();  
-	// loop through all the xml nodes retrieving the content
-	for(i=0;i<resultsXml.length;i++){
-		resultsArray[i] = {};
-		resultsArray[i].bar_code = resultsXml.item(i).getElementsByTagName('bar_code')[0].firstChild.data;
-		resultsArray[i].name = resultsXml.item(i).getElementsByTagName('name')[0].firstChild.data;
-		resultsArray[i].packaging = resultsXml.item(i).getElementsByTagName('packaging')[0].firstChild.data;
-	}
-	// return the node's content as an array
-	return resultsArray;
 }
 
 /**
@@ -545,89 +544,78 @@ SearchProduct.prototype.handleKeyDown = function(oEvent){
 	code = (oEvent.keyCode) ? oEvent.keyCode :
 			((oEvent.which) ? oEvent.which : 0);
 	// check to see if the event was keyup
-	if (oEvent.type == "keydown") 
+	if (oEvent.type == 'keydown') 
 	{    
-		this._mIsKeyUpDownPressed =false; 
-		// check to see we if are interested in the current character
-		if ((code < 13 && code != 8) || 
-				(code >=14 && code < 32) || 
-				(code >= 33 && code <= 46 && code != 38 && code != 40) || 
-				(code >= 112 && code <= 123)) 
-		{
-			// simply ignore non-interesting characters
-		}
-		else
-			/* if Enter is pressed we jump to the PHP help page of the current 
-       		function */
-			if(code == 13)
+		this._mIsKeyUpDownPressed = false; 
+		/* if Enter is pressed we jump to the PHP help page of the current 
+   		function */
+		if(code == 13)
+			// check to see if any function is currently selected
+			if(this._mPosition >= 0)
 			{
-				// check to see if any function is currently selected
-				if(this._mPosition>=0)
-				{
-					var oTr = document.getElementById('tr' + this._mPosition);
-					var crtLink = oTr.getElementsByTagName('TD')[1].id;
-					oSession.loadHref('index.php?cmd=get_product_by_bar_code&bar_code=' + crtLink);
-				}
-			}        
-			else
-				// if the down arrow is pressed we go to the next suggestion
-				if(code == 40)
-				{                   
-					newTR=document.getElementById("tr"+(++this._mPosition));
-					oldTR=document.getElementById("tr"+(--this._mPosition));
-					// deselect the old selected suggestion   
-					if(this._mPosition>=0 && this._mPosition<this._mSuggestions-1)
-						oldTR.className = "";
+				var oTr = document.getElementById('tr' + this._mPosition);
+				var crtLink = oTr.getElementsByTagName('TD')[1].id;
+				oSession.loadHref('index.php?cmd=get_product_by_bar_code&bar_code=' + crtLink);
+			}
+
+		// if the down arrow is pressed we go to the next suggestion
+		if(code == 40)
+		{                   
+			newTR=document.getElementById('tr' + (++this._mPosition));
+			oldTR=document.getElementById('tr' + (--this._mPosition));
+			// deselect the old selected suggestion   
+			if(this._mPosition >= 0 && this._mPosition < this._mSuggestions - 1)
+				oldTR.className = '';
  
 					// select the new suggestion and update the keyword
-					if(this._mPosition < this._mSuggestions - 1)
-					{
-						newTR.className = "highlightrow";
-						this.updateKeywordValue(newTR);
-						this._mPosition++;         
-					}     
-					oEvent.cancelBubble = true;
-					oEvent.returnValue = false;
-					this._mIsKeyUpDownPressed = true;        
-					// scroll down if the current window is no longer valid
-					if(this._mPosition > this._mMaxVisiblePosition)
-					{   
-						this._mScrollDiv.scrollTop += 18;
-						this._mMaxVisiblePosition += 1;
-						this._mMinVisiblePosition += 1;
-					}
-				}
-				else
-					// if the up arrow is pressed we go to the previous suggestion
-					if(code == 38)
-					{       
-						newTR=document.getElementById("tr"+(--this._mPosition));
-						oldTR=document.getElementById("tr"+(++this._mPosition));
-						// deselect the old selected this._mPosition
-						if(this._mPosition>=0 && this._mPosition <= this._mSuggestions - 1)
-						{       
-							oldTR.className = "";
-						}
-						// select the new suggestion and update the keyword
-						if(this._mPosition > 0)
-						{
-							newTR.className = "highlightrow";
-							this.updateKeywordValue(newTR);
-							this._mPosition--;
-							// scroll up if the current window is no longer valid
-							if(this._mPosition<this._mMinVisiblePosition)
-							{
-								this._mScrollDiv.scrollTop -= 18;
-								this._mMaxVisiblePosition -= 1;
-								this._mMinVisiblePosition -= 1;
-							}   
-						}     
-						else
-							if(this._mPosition == 0)
-								this._mPosition--;
-						oEvent.cancelBubble = true;
-						oEvent.returnValue = false;
-						this._mIsKeyUpDownPressed = true;  
-					}     
+			if(this._mPosition < this._mSuggestions - 1)
+			{
+				newTR.className = 'highlightrow';
+				this.updateKeywordValue(newTR);
+				this._mPosition++;
+			}
+			oEvent.cancelBubble = true;
+			oEvent.returnValue = false;
+			this._mIsKeyUpDownPressed = true;        
+			// scroll down if the current window is no longer valid
+			if(this._mPosition > this._mMaxVisiblePosition)
+			{   
+				this._mScrollDiv.scrollTop += 18;
+				this._mMaxVisiblePosition += 1;
+				this._mMinVisiblePosition += 1;
+			}
+		}
+
+		// if the up arrow is pressed we go to the previous suggestion
+		if(code == 38)
+		{       
+			newTR=document.getElementById('tr' + (--this._mPosition));
+			oldTR=document.getElementById('tr' + (++this._mPosition));
+			// deselect the old selected this._mPosition
+			if(this._mPosition >= 0 && this._mPosition <= this._mSuggestions - 1)
+			{       
+				oldTR.className = '';
+			}
+			// select the new suggestion and update the keyword
+			if(this._mPosition > 0)
+			{
+				newTR.className = 'highlightrow';
+				this.updateKeywordValue(newTR);
+				this._mPosition--;
+				// scroll up if the current window is no longer valid
+				if(this._mPosition < this._mMinVisiblePosition)
+				{
+					this._mScrollDiv.scrollTop -= 18;
+					this._mMaxVisiblePosition -= 1;
+					this._mMinVisiblePosition -= 1;
+				}   
+			}     
+			else
+				if(this._mPosition == 0)
+					this._mPosition--;
+			oEvent.cancelBubble = true;
+			oEvent.returnValue = false;
+			this._mIsKeyUpDownPressed = true;  
+		}
 	}
 }
