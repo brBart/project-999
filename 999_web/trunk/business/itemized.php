d868319d947bec639ae7860b91a84f0fbfaf9b5c
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-09-11 19:16:00 +0000

    The showLasPage method added to the DetailsPrinter class. Documentation fixed also.

--- 999_web/trunk/business/itemized.php
+++ 999_web/trunk/business/itemized.php
@@ -27,15 +27,15 @@ class DetailsPrinter{
 	/**
 	 * Returns the array with the details of the requested page of the provided object.
 	 *
-	 * If no page argument or cero is passed all the details are returned. The total_pages and
-	 * total_items arguments are necessary to return their respective values.
+	 * If no page argument or cero is passed all the details are returned. The totalPages and
+	 * totalItems arguments are necessary to return their respective values.
 	 * @param Itemized $obj
-	 * @param integer &$total_pages
-	 * @param integer &$total_items
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
 	 * @param integer $page
 	 * @return array
 	 */
-	static public function showPage(Itemized $obj, &$total_pages = 0, &$total_items = 0, $page = 0){
+	static public function showPage(Itemized $obj, &$totalPages = 0, &$totalItems = 0, $page = 0){
 		if($page !== 0)
 			Number::validatePositiveInteger($page, 'Pagina inv&aacute;lida.');
 		
@@ -51,19 +51,47 @@ class DetailsPrinter{
 			foreach($obj_details as $detail)
 				$details[] = $detail->show();
 		else{
-			$total_items = count($obj_details);
-			$total_pages = ceil($total_items / ITEMS_PER_PAGE);
+			$totalItems = count($obj_details);
+			$totalPages = ceil($totalItems / ITEMS_PER_PAGE);
 			
 			$first_item = ($page - 1) * ITEMS_PER_PAGE;
 			
-			if($page == $total_pages)
-				$last_item = $total_items;
+			if($page == $totalPages)
+				$last_item = $totalItems;
 			else
 				$last_item = $first_item + ITEMS_PER_PAGE;
 				
 			for($i = $first_item; $i < $last_item; $i++)
 				$details[] = $obj_details[$i]->show();
 		}
+		
+		return $details;
+	}
+	
+	/**
+	 * Returns the array with the details of the last page of the provided object.
+	 *
+	 * The totalPages and totalItems arguments are necessary to return their respective values.
+	 * @param Itemized $obj
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
+	 * @return array
+	 */
+	static public function showLastPage(Itemized $obj, &$totalPages = 0, &$totalItems = 0){
+		$obj_details = $obj->getDetails();
+		$details = array();
+		
+		// If there are no details.
+		if(empty($obj_details))
+			return $details;
+		
+		$totalItems = count($obj_details);
+		$totalPages = ceil($totalItems / ITEMS_PER_PAGE);
+		
+		$first_item = ($totalPages - 1) * ITEMS_PER_PAGE;
+			
+		for($i = $first_item; $i < $totalItems; $i++)
+			$details[] = $obj_details[$i]->show();
 		
 		return $details;
 	}
