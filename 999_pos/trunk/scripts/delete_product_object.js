6e3699a08c84e434dd98b9a3262c37036b152d4d
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-10-06 18:40:35 +0000

    Documentation fix.

--- 999_pos/trunk/scripts/delete_product_object.js
+++ 999_pos/trunk/scripts/delete_product_object.js
@@ -1,16 +1,17 @@
 /**
- * Library with the delete product object command class.
- * @package Client
+ * @fileOverview Library with the DeleteProductObjectCommand class.
  * @author Roberto Oliveros
  */
 
 /**
- * Constructor function.
- * @param Session oSession
- * @param Console oConsole
- * @param Request oRequest
- * @param string sKey
- * @param DocumentPage oDetails
+ * @class Deletes a product from an object on the server.
+ * @extends DeleteDetailCommand
+ * @constructor
+ * @param {Session} oSession
+ * @param {Console} oConsole
+ * @param {XmlHttpRequest} oRequest
+ * @param {String} sKey
+ * @param {DocumentPage} oDetails
  */
 function DeleteProductObjectCommand(oSession, oConsole, oRequest, sKey, oDetails){
 	// Call the parent constructor.
@@ -23,9 +24,9 @@ function DeleteProductObjectCommand(oSession, oConsole, oRequest, sKey, oDetails
 DeleteProductObjectCommand.prototype = new DeleteDetailCommand();
 
 /**
-* Method for displaying success.
-* @param DocumentElement xmlDoc
-*/
+ * Method for displaying success.
+ * @param {DocumentElement} xmlDoc
+ */
 DeleteProductObjectCommand.prototype.displaySuccess = function(xmlDoc){
 	this._mDetails.updatePage();
 	this._mDetails.setFocus();
