2dfd3102361c92c135af147caa7487f6d0b56637
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-10-07 19:36:16 +0000

    Template manufacturer_menu renamed to object_menu so it can be reused. Also the show_unit_of_measure command was implemented.

--- 999_pos/trunk/commands/show_unit_of_measure_menu.php
+++ 999_pos/trunk/commands/show_unit_of_measure_menu.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Library containing the ShowManufacturerMenu command.
+ * Library containing the ShowUnitOfMeasureMenu command.
  * @package Command
  * @author Roberto Oliveros
  */
@@ -15,23 +15,23 @@ require_once('presentation/command.php');
 require_once('presentation/page.php');
 
 /**
- * Command to display the manufacturers menu.
+ * Command to display the unit of measure menu.
  * @package Command
  * @author Roberto Oliveros
  */
-class ShowManufacturerMenuCommand extends Command{
+class ShowUnitOfMeasureMenuCommand extends Command{
 	/**
 	 * Execute the command.
 	 * @param Request $request
 	 * @param SessionHelper $helper
 	 */
 	public function execute(Request $request, SessionHelper $helper){
-		$back_trace = array('Inicio', 'Mantenimiento', 'Casas');
+		$back_trace = array('Inicio', 'Mantenimiento', 'Unidades de Medida');
 		Page::display(array('module_title' => OPERATIONS_TITLE, 'main_menu' => 'main_menu_operations_html.tpl',
 				'back_trace' => $back_trace, 'second_menu' => 'maintenance_menu_operations_html.tpl',
 				'content' => 'object_menu_html.tpl', 'notify' => '0',
-				'create_link' => 'index.php?cmd=create_manufacturer',
-				'show_list_link' => 'index.php?cmd=show_manufacturer_list&page=1'), 'site_html.tpl');
+				'create_link' => 'index.php?cmd=create_unit_of_measure',
+				'show_list_link' => 'index.php?cmd=show_unit_of_measure_list&page=1'), 'site_html.tpl');
 	}
 }
 ?>
--- 999_pos/trunk/templates/manufacturer_menu_html.tpl
+++
@@ -1,7 +0,0 @@
-{* Smarty * }
-<div id="third_menu">
-	<ul>
-	    <li><a href="index.php?cmd=create_manufacturer" onclick="return oSession.setIsLink(true);">Crear</a></li>
-	    <li id="li_last"><a href="index.php?cmd=show_manufacturer_list&page=1" onclick="return oSession.setIsLink(true);">Consultar</a></li>
-	</ul>
-</div>
