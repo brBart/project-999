940542598840c74bb39915b3fc15591e2581fea9
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-10-07 01:30:16 +0000

    Documentation fix.

--- 999_pos/trunk/scripts/session.js
+++ 999_pos/trunk/scripts/session.js
@@ -1,50 +1,57 @@
 /**
- * Library with the object in charge for keeping the status of the actual session.
+ * @fileOverview Library with the Session class.
+ * @author Roberto Oliveros
  */
 
 /**
- * Constructor function, session flag is true by default.
+ * @class Class in charge for keeping track of the status of the actual session.
+ * @constructor
  */
 function Session(){
 	/**
 	 * Flag to keep track of the actual session.
+	 * @type Boolean
 	 */
 	this._mIsActive = true;
 	
 	/**
-	 * Flag to keep track of the system flow. False by default, meaning that if the system closes it wasn't by
-	 * the logout link. 
+	 * Flag to keep track of the system navigation flow. False by default, meaning that if the system closes it wasn't by
+	 * the logout link.
+	 * @type Boolean 
 	 */
 	this._mIsLink = false;
 }
 
 /**
  * Sets the value of the session flag.
- * @param boolean bValue
+ * @param {Boolean} bValue
  */
 Session.prototype.setIsActive = function(bValue){
 	this._mIsActive = bValue;
 }
 
 /**
- * Used by all the system's links to notify... they have been click. Always return true to keep up with the link.
- * @param boolean bValue
- * @return boolean
+ * Used by all the system's links to notify they have been click. Always return true to keep up with the link.
+ * @param {Boolean} bValue
+ * @returns {Boolean}
  */
 Session.prototype.setIsLink = function(bValue){
 	this._mIsLink = bValue;
 	return true;
 }
  
-
+/**
+ * Redirects the html document to the provided href.
+ * @param {String} sHref
+ */
 Session.prototype.loadHref = function(sHref){
 	this._mIsLink = true;
 	document.location.href = sHref;
 }
 
 /**
- * Check before unloading the current page if the event was a system click event or other thing.
- * @param event oEvent
+ * Check before unloading the current page if the event was a navigation click event or other thing.
+ * @param {Event} oEvent
  */
 Session.prototype.verifyStatus = function(oEvent){
 	// It was a click on a system link.
