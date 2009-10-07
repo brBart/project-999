1e3ab6da21a65bca54538f1a0d6ae2cfea1dfbba
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-10-07 22:17:45 +0000

    The DeleteUnitOfMeasureCommand implemented.

---
+++ 999_pos/trunk/commands/delete_unit_of_measure.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Library containing the DeleteUnitOfMeasure command.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('commands/delete_object.php');
+
+/**
+ * Implements functionality for deleting unit of measure objects.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class DeleteUnitOfMeasureCommand extends DeleteObjectCommand{
+	/**
+	 * Tests if the user has the right to delete the object.
+	 * @param UserAccount $user
+	 * @return boolean
+	 */
+	protected function testRights(UserAccount $user){
+		return AccessManager::isAllowed($user, 'unit_of_measure', 'write');
+	}
+	
+	/**
+	 * Deletes the desired object.
+	 * @param variant $obj
+	 */
+	protected function deleteObject($obj){
+		UnitOfMeasure::delete($obj);
+	}
+}
+?>
