5e9548a7f846ea38819819626c0e1827442093a3
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-10-14 20:06:26 +0000

    The EntryAdjustmentEvent was removed.

--- 999_web/branches/Busines_Finished_Stable/business/event.php
+++ 999_web/branches/Busines_Finished_Stable/business/event.php
@@ -44,65 +44,6 @@ class EntryEvent{
 
 
 /**
- * Event class for creating entry type documents and adjust negative lots in the inventory.
- * @package Event
- * @author Roberto Oliveros
- */
-class EntryAdjustmentEvent extends EntryEvent{
-	/**
-	 * Adds a detail to the provided document with the provided data.
-	 *
-	 * If there are negative lots of the product provided in the inventory, these will be adjusted and no lots
-	 * will be created. The msg parameter is to pass back the notification to the user. Date format: 'dd/mm/yyyy'.
-	 * @param Product $product
-	 * @param Document $document
-	 * @param integer $quantity
-	 * @param float $price
-	 * @param string $expirationDate
-	 * @param string &$msg
-	 */
-	static public function apply(Product $product, Document $document, $quantity, $price,
-			$expirationDate = NULL, &$msg = NULL){
-		Persist::validateNewObject($document);
-				
-		$lots = Inventory::getNegativeLots($product, $quantity);
-		
-		foreach($lots as &$lot){
-			$negative = $lot->getQuantity();
-			if($negative < 0){
-				
-				if(abs($negative) <= $quantity){
-					$lot_quantity = abs($negative);
-					$quantity -= $lot_quantity;
-					// Adjust the lot's quantity
-					$lot->increase($lot_quantity);
-					// Set the negative quantity in case the action will be cancel.
-					$lot->setNegativeQuantity($negative);
-				}
-				else{
-					$lot_quantity = $quantity;
-					$lot->increase($lot_quantity);
-					$lot->setNegativeQuantity(-1 * $lot_quantity);
-				}
-				
-				$msg = 'Lote ' . $lot->getId() . ' del producto agregado contiene un saldo negativo. No se ha ' .
-						'creado un nuevo lote para poder este ser ajustado.';
-			}
-			else{
-				// Lot is not a NegativeLot.
-				$lot_quantity = $quantity;
-				$lot->increase($lot_quantity);
-			}
-			
-			$lot->setPrice($price);
-			$lot->setExpirationDate($expirationDate);
-			$document->addDetail(new DocProductDetail($lot, new Entry(), $lot_quantity, $price));
-		}
-	}
-}
-
-
-/**
  * Event class for creating withdraw type documents.
  * @package Event
  * @author Roberto Oliveros
