0962bbca737b10a13f5f1b62cc16ca08eeb61814
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-10-31 22:50:19 +0000

    The ShowInventoryMenuCommand class implemented.

---
+++ 999_pos/trunk/commands/show_inventory_menu.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Library containing the ShowInventoryMenuCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+
+/**
+ * Command to display the inventory menu.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class ShowInventoryMenuCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$back_trace = array('Inicio', 'Inventariados');
+		Page::display(array('module_title' => OPERATIONS_TITLE, 'main_menu' => 'main_menu_operations_html.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'inventory_menu_html.tpl',
+				'content' => 'none', 'notify' => '0'), 'site_html.tpl');
+	}
+}
+?>
---
+++ 999_pos/trunk/templates/inventory_menu_html.tpl
@@ -0,0 +1,6 @@
+{* Smarty * }
+<ul>
+    <li><a href="index.php?cmd=show_counting_template_menu" onclick="return oSession.setIsLink(true);">Selectivos</a></li>
+    <li><a href="index.php?cmd=show_counting_menu" onclick="return oSession.setIsLink(true);">Conteos</a></li>
+    <li><a href="index.php?cmd=show_comparison_menu" onclick="return oSession.setIsLink(true);">Comparaciones</a></li>
+</ul>
