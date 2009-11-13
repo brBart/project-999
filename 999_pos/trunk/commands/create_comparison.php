4980f61876e1fc2064f58a24632570a7c5fdfa7b
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-11-13 01:57:39 +0000

    First commit.

---
+++ 999_pos/trunk/commands/create_comparison.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Library containing the CreateComparisonCommand base class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+
+/**
+ * Defines functionality for creating a comparison.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class CreateComparisonCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		
+	}
+}
+?>
