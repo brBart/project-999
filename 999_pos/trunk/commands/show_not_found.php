6ca86efedb55c067afdf3d5daec9e0f57d08e25b
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-12-01 18:19:28 +0000

    The ShowNotFoundAdminCommand class implemented with all the consequences.

---
+++ 999_pos/trunk/commands/show_not_found.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Library containing the ShowNotFoundCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+
+/**
+ * Defines common functionality for displaying a not found command failure.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+abstract class ShowNotFoundCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$msg = 'Interno: Comando no existe.';
+		$type = $request->getProperty('type');
+		
+		if($type == 'xml')
+			// Is an ajax request.
+			Page::display(array('message' => $msg), 'error_xml.tpl');
+		else
+			$this->displayFailure($msg);
+	}
+	
+	/**
+	 * Displays the failure message to the user in html format.
+	 * @param string $msg
+	 */
+	abstract protected function displayFailure($msg);
+}
+?>
--- 999_pos/trunk/commands/show_not_found_admin.php
+++ 999_pos/trunk/commands/show_not_found_admin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Library containing the ShowNotFoundOperationsCommand class.
+ * Library containing the ShowNotFoundAdminCommand class.
  * @package Command
  * @author Roberto Oliveros
  */
@@ -15,19 +15,19 @@ require_once('commands/show_not_found.php');
 require_once('presentation/page.php');
 
 /**
- * Command to display the not found message on the operations site.
+ * Command to display the not found message on the administration site.
  * @package Command
  * @author Roberto Oliveros
  */
-class ShowNotFoundOperationsCommand extends ShowNotFoundCommand{
+class ShowNotFoundAdminCommand extends ShowNotFoundCommand{
 	/**
 	 * Displays the failure message to the user in html format.
 	 * @param string $msg
 	 */
 	protected function displayFailure($msg){
 		$back_trace = array('Inicio');
-		Page::display(array('module_title' => OPERATIONS_TITLE,
-				'main_menu' => 'main_menu_operations_html.tpl', 'back_trace' => $back_trace,
+		Page::display(array('module_title' => ADMIN_TITLE,
+				'main_menu' => 'main_menu_admin_html.tpl', 'back_trace' => $back_trace,
 				'second_menu' => 'none', 'content' => 'none', 'notify' => '1', 'type' => 'error',
 				'message' => $msg), 'site_html.tpl');
 	}
