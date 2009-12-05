715138168bbf94889f797f27eb5cb549489d73ee
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-12-05 02:30:57 +0000

    First commit.

---
+++ 999_pos/trunk/commands/delete_user_account.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Library containing the DeleteUserAccountCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('commands/delete_object.php');
+
+/**
+ * Implements functionality for deleting user account objects.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class DeleteUserAccountCommand extends DeleteObjectCommand{
+	/**
+	 * Tests if the user has the right to delete the object.
+	 * @param UserAccount $user
+	 * @return boolean
+	 */
+	protected function testRights(UserAccount $user){
+		return AccessManager::isAllowed($user, 'user_account', 'write');
+	}
+	
+	/**
+	 * Deletes the desired object.
+	 * @param variant $obj
+	 */
+	protected function deleteObject($obj){
+		UserAccount::delete($obj);
+	}
+}
+?>
