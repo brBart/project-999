40229e1189994b69fc9c007446a483acffa12110
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-12-28 23:16:53 +0000

    First commit.

---
+++ 999_pos/trunk/commands/show_shift_list.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Library containing the ShowShiftListCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('commands/show_list.php');
+/**
+ * For obtaining the list.
+ */
+require_once('business/list.php');
+
+/**
+ * Implements functionality for showing the shift list.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class ShowShiftListCommand extends ShowListCommand{
+	/**
+	 * Returns a list with information.
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
+	 * @param integer $page
+	 * @return array
+	 */
+	protected function getList(&$totalPages, &$totalItems, $page){
+		return ShiftList::getList($totalPages, $totalItems, $page);
+	}
+	
+	/**
+	 * Displays an empty list.
+	 */
+	protected function displayEmpty(){
+		$back_trace = array('Inicio', 'Mantenimiento', 'Turnos de Caja');
+		$msg = 'No hay turnos de caja en la base de datos.';
+		Page::display(array('module_title' => ADMIN_TITLE, 'main_menu' => 'back_link.tpl',
+				'back_link' => 'index.php?cmd=show_shift_menu', 'back_trace' => $back_trace,
+				'second_menu' => 'none', 'content' => 'none', 'notify' => '1', 'type' => 'info',
+				'message' => $msg), 'site_html.tpl');
+	}
+	
+	/**
+	 * Displays the list.
+	 * @param array $list
+	 * @param integer $totalPages
+	 * @param integer $totalItems
+	 * @param integer $page
+	 * @param integer $firstItem
+	 * @param integer $lastItem
+	 * @param string $previousLink
+	 * @param string $nextLink
+	 * @param string $actualCmd
+	 */
+	protected function displayList($list, $totalPages, $totalItems, $page, $firstItem, $lastItem, $previousLink,
+			$nextLink, $actualCmd){
+		$back_trace = array('Inicio', 'Mantenimiento', 'Turnos de Caja');
+		Page::display(array('module_title' => ADMIN_TITLE, 'main_menu' => 'back_link.tpl',
+				'back_link' => 'index.php?cmd=show_shift_menu', 'back_trace' => $back_trace,
+				'second_menu' => 'none', 'content' => 'identifier_list_html.tpl', 'list' => $list,
+				'total_items' => $totalItems, 'total_pages' => $totalPages, 'page' => $page,
+				'first_item' => $firstItem, 'last_item' => $lastItem, 'previous_link' => $previousLink,
+				'next_link' => $nextLink, 'item_link' => 'index.php?cmd=get_shift&id=',
+				'actual_cmd' => $actualCmd), 'site_html.tpl');
+	}
+}
+?>
