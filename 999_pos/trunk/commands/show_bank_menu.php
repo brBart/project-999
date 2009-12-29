0122ba4c6d7b32058efd4dae7a61c780eb9e2ed9
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-12-29 22:49:07 +0000

    First commit.

---
+++ 999_pos/trunk/commands/show_bank_menu.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Library containing the ShowBankMenuCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+
+/**
+ * Command to display the bank menu.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class ShowBankMenuCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$back_trace = array('Inicio', 'Mantenimiento', 'Bancos');
+		Page::display(array('module_title' => ADMIN_TITLE, 'main_menu' => 'main_menu_admin_html.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'maintenance_menu_admin_html.tpl',
+				'content' => 'object_menu_html.tpl', 'create_link' => 'index.php?cmd=create_bank',
+				'show_list_link' => 'index.php?cmd=show_bank_list&page=1'), 'site_html.tpl');
+	}
+}
+?>
