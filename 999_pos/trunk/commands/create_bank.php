f96a8ea4a6068bdbec87b5698521d47a362aa1f1
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-12-29 23:19:59 +0000

    First commit.

---
+++ 999_pos/trunk/commands/create_bank.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Library containing the CreateBankCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('commands/create_object.php');
+/**
+ * Library with the bank class.
+ */
+require_once('business/cash.php');
+
+/**
+ * Displays the bank form in edit mode.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class CreateBankCommand extends CreateObjectCommand{
+	/**
+	 * Tests if the user has the right to create the object.
+	 * @param UserAccount $user
+	 * @return boolean
+	 */
+	protected function testRights(UserAccount $user){
+		return AccessManager::isAllowed($user, 'bank', 'write');
+	}
+	
+	/**
+	 * Display failure in case the user doesn't have rights.
+	 */
+	protected function displayFailure(){
+		$back_trace = array('Inicio', 'Mantenimiento', 'Bancos');
+		$msg = 'Usuario no cuenta con los suficientes privilegios.';
+		Page::display(array('module_title' => ADMIN_TITLE, 'main_menu' => 'main_menu_admin_html.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'maintenance_menu_admin_html.tpl',
+				'content' => 'object_menu_html.tpl', 'notify' => '1', 'type' => 'error', 'message' => $msg,
+				'create_link' => 'index.php?cmd=create_bank',
+				'show_list_link' => 'index.php?cmd=show_bank_list&page=1'), 'site_html.tpl');
+	}
+	
+	/**
+	 * Creates the desired object.
+	 * @return variant
+	 */
+	protected function createObject(){
+		return new Bank();
+	}
+	
+	/**
+	 * Display the form for creating the object.
+	 * @param string $key
+	 * @param variant $obj
+	 */
+	protected function displayObject($key, $obj){
+		$back_trace = array('Inicio', 'Mantenimiento', 'Bancos');
+		Page::display(array('module_title' => ADMIN_TITLE, 'main_menu' => 'blank.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'none',
+				'content' => 'identifier_form_html.tpl', 'status' => '0', 'key' => $key,
+				'back_link' => 'index.php?cmd=show_bank_menu', 'foward_link' => 'index.php?cmd=get_bank'),
+				'site_html.tpl');
+	}
+}
+?>
