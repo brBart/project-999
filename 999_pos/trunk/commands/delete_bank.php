ffd46324742a3a9fc9c08470c38067f057a95fbf
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2009-12-30 00:00:47 +0000

    First commit.

---
+++ 999_pos/trunk/commands/delete_bank.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Library containing the DeleteBankCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('commands/delete_object.php');
+
+/**
+ * Implements functionality for deleting bank objects.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class DeleteBankCommand extends DeleteObjectCommand{
+	/**
+	 * Tests if the user has the right to delete the object.
+	 * @param UserAccount $user
+	 * @return boolean
+	 */
+	protected function testRights(UserAccount $user){
+		return AccessManager::isAllowed($user, 'shift', 'write');
+	}
+	
+	/**
+	 * Deletes the desired object.
+	 * @param variant $obj
+	 */
+	protected function deleteObject($obj){
+		Bank::delete($obj);
+	}
+}
+?>
