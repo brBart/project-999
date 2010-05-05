0f449146f97b6d16034787eb6779178c8d645cad
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-05-05 00:47:03 +0000

    First commit.

---
+++ 999_exe/trunk/cash_register_dialog/cash_register_dialog.cpp
@@ -0,0 +1,50 @@
+#include "cash_register_dialog.h"
+
+#include "../xml_transformer/map_string_xml_transformer.h"
+#include <QMessageBox>
+
+CashRegisterDialog::CashRegisterDialog(QNetworkAccessManager *manager, QUrl *url,
+		QWidget *parent) : QDialog(parent), m_ServerUrl(url)
+{
+	ui.setupUi(this);
+
+	ui.webView->setHtml("<div id=\"console\" style=\"color: red;\"></div>");
+	m_Console = new Console(ui.webView->page()->mainFrame());
+
+	m_Request = new HttpRequest(manager, this);
+
+	m_Handler = new XmlResponseHandler(this);
+
+	loadShifts();
+}
+
+CashRegisterDialog::~CashRegisterDialog()
+{
+	delete m_Console;
+}
+
+void CashRegisterDialog::loadShifts()
+{
+	QUrl url(*m_ServerUrl);
+	url.addQueryItem("cmd", "get_shift_list");
+	url.addQueryItem("type", "xml");
+
+	QString content = m_Request->get(url);
+
+	QString errorMsg;
+	MapStringXmlTransformer *transformer = new MapStringXmlTransformer();
+	if (m_Handler->handle(content, transformer, &errorMsg) ==
+			XmlResponseHandler::Success) {
+		QList<QMap<QString, QString>*> list = transformer->list();
+
+		ui.shiftsComboBox->addItem("", "");
+		QMap<QString, QString> *shift;
+		for (int i = 0; i < list.size(); i++) {
+			shift = list[i];
+			ui.shiftsComboBox->addItem(shift->value("name"),
+					shift->value("shift_id"));
+		}
+	} else {
+		m_Console->displayError(errorMsg);
+	}
+}
---
+++ 999_exe/trunk/cash_register_dialog/cash_register_dialog.h
@@ -0,0 +1,32 @@
+#ifndef CASH_REGISTER_DIALOG_H
+#define CASH_REGISTER_DIALOG_H
+
+#include <QtGui/QDialog>
+#include "ui_cash_register_dialog.h"
+
+#include <QNetworkAccessManager>
+#include <QUrl>
+#include "../console/console.h"
+#include "../http_request/http_request.h"
+#include "../xml_response_handler/xml_response_handler.h"
+
+class CashRegisterDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    CashRegisterDialog(QNetworkAccessManager *manager, QUrl *url,
+    		QWidget *parent = 0);
+    ~CashRegisterDialog();
+
+private:
+    Ui::CashRegisterDialogClass ui;
+    QUrl *m_ServerUrl;
+    Console *m_Console;
+    HttpRequest *m_Request;
+    XmlResponseHandler *m_Handler;
+
+    void loadShifts();
+};
+
+#endif // CASH_REGISTER_DIALOG_H
