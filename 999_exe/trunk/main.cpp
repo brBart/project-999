ee3f5878cb18689df675d6ca5a1c732f67c3022b
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-05-11 19:05:11 +0000

    QTransalator installed.

--- 999_exe/trunk/main.cpp
+++ 999_exe/trunk/main.cpp
@@ -2,11 +2,18 @@
 
 #include <QtGui>
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    translator.load("qt_es");
+    a.installTranslator(&translator);
+
     MainWindow w;
     w.showMaximized();
+
     return a.exec();
 }
