f95829d3435c0eeb9c0984bd732ef40d02eec916
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-05-11 19:06:06 +0000

    First commit.

---
+++ 999_exe/trunk/recordset/recordset.cpp
@@ -0,0 +1,70 @@
+#include "recordset.h"
+
+Recordset::Recordset(QWidget *parent)
+    : QWidget(parent)
+{
+	ui.setupUi(this);
+}
+
+Recordset::~Recordset()
+{
+	delete m_Iterator;
+}
+
+void Recordset::setList(QList<QMap<QString, QString>*> list)
+{
+	m_List = list;
+	m_Iterator = new QListIterator<QMap<QString, QString>*>(m_List);
+}
+
+int Recordset::size()
+{
+	return m_List.size();
+}
+
+void Recordset::moveFirst()
+{
+	m_Iterator->toFront();
+	QMap<QString, QString> *record = m_Iterator->next();
+	m_Index = 1;
+
+	emit recordChanged(record->value("id"));
+
+	updateLabel();
+}
+
+void Recordset::movePrevious()
+{
+	QMap<QString, QString> *record = m_Iterator->previous();
+	m_Index = m_Index - 1;
+
+	emit recordChanged(record->value("id"));
+
+	updateLabel();
+}
+
+void Recordset::moveNext()
+{
+	QMap<QString, QString> *record = m_Iterator->next();
+	m_Index = m_Index + 1;
+
+	emit recordChanged(record->value("id"));
+
+	updateLabel();
+}
+
+void Recordset::moveLast()
+{
+	m_Iterator->toBack();
+	QMap<QString, QString> *record = m_Iterator->previous();
+	m_Index = m_List.size();
+
+	emit recordChanged(record->value("id"));
+
+	updateLabel();
+}
+
+void Recordset::updateLabel()
+{
+	ui.label->setText(QString("%1 de %2").arg(m_Index).arg(m_List.size()));
+}
---
+++ 999_exe/trunk/recordset/recordset.h
@@ -0,0 +1,38 @@
+#ifndef RECORDSET_H
+#define RECORDSET_H
+
+#include <QtGui/QWidget>
+#include "ui_recordset.h"
+
+#include <QList>
+#include <QMap>
+
+class Recordset : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Recordset(QWidget *parent = 0);
+    ~Recordset();
+    void setList(QList<QMap<QString, QString>*> list);
+    int size();
+
+public slots:
+	void moveFirst();
+	void movePrevious();
+	void moveNext();
+	void moveLast();
+
+signals:
+	void recordChanged(QString id);
+
+private:
+    Ui::RecordsetClass ui;
+    QList<QMap<QString, QString>*> m_List;
+    QListIterator<QMap<QString, QString>*> *m_Iterator;
+    int m_Index;
+
+    void updateLabel();
+};
+
+#endif // RECORDSET_H
