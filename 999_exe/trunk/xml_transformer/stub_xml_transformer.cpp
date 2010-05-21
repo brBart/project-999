b85f5f2c2bbb1a80277bf768accabe6a5fd4aa1c
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-05-21 22:59:45 +0000

    First commit.

---
+++ 999_exe/trunk/xml_transformer/stub_xml_transformer.cpp
@@ -0,0 +1,16 @@
+/*
+ * stub_xml_transformer.cpp
+ *
+ *  Created on: 21/05/2010
+ *      Author: pc
+ */
+
+#include "stub_xml_transformer.h"
+
+/**
+ * Transforms the xml document.
+ */
+void StubXmlTransformer::transform(QDomDocument *document)
+{
+
+}
