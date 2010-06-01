c802f21b4a9fe2b9ed4c96db90f70d76e6ebf7fb
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-06-01 16:11:26 +0000

    First commit.

---
+++ 999_pos/trunk/commands/get_invoice_details.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Library containing the GetInvoiceDetailsCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+/**
+ * For displaying the object details.
+ */
+require_once('business/itemized.php');
+
+/**
+ * Defines functionality for obtaining an invoice's details.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class GetInvoiceDetailsCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$invoice = $helper->getObject((int)$request->getProperty('key'));
+		$details = DetailsPrinter::showPage($invoice);
+		Page::display(array('details' => $details, 'sub_total' => $invoice->getSubTotal(),
+				'discount' => $invoice->getTotalDiscount(), 'total' => $invoice->getTotal()),
+				'invoice_details_xml.tpl');
+	}
+}
+?>
---
+++ 999_pos/trunk/templates/invoice_details_xml.tpl
@@ -0,0 +1,27 @@
+{* Smarty *}
+{php}
+header('Content-Type: text/xml');
+{/php}
+<?xml version="1.0" encoding="UTF-8"?>
+<response>
+	<success>1</success>
+	<params>
+		<sub_total>{$sub_total|nf:2}</sub_total>
+		<discount>{$discount|nf:2}</discount>
+		<total>{$total|nf:2}</total>
+	</params>
+	<grid>
+		{section name=i loop=$details}
+		<row>
+			<detail_id>{$details[i].id}</detail_id>
+			<bar_code><![CDATA[{$details[i].bar_code}]]></bar_code>
+			<manufacturer><![CDATA[{$details[i].manufacturer}]]></manufacturer>
+			<product><![CDATA[{$details[i].product}]]></product>
+			<packaging><![CDATA[{$details[i].packaging}]]></packaging>
+			<quantity>{$details[i].quantity}</quantity>
+			<price>{$details[i].price|nf:2}</price>
+			<total>{$details[i].total|nf:2}</total>
+		</row>
+		{/section}
+	</grid>
+</response>
