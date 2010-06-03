affb44833d97261de5f9151f47626b26f698b5dd
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-06-03 01:34:26 +0000

    The setFrame method override because the main frame is needed.

--- 999_exe/trunk/console/html_console.cpp
+++ 999_exe/trunk/console/html_console.cpp
@@ -6,18 +6,26 @@
  */
 
 #include "html_console.h"
-
 /**
  * @class HtmlConsole
  * Use to display messages to the user using a html page.
  */
 
 /**
+ * Sets the main frame and then calls its parent's setFrame method.
+ */
+void HtmlConsole::setFrame(QWebFrame *frame)
+{
+	m_Frame = frame;
+	Console::setFrame(frame);
+}
+
+/**
  * Changes the element's style class from "failed" to "hidden".
  */
 void HtmlConsole::hideElementIndicator(QString elementId)
 {
-	QWebElement element = m_Div.findFirst("#" + elementId + "-failed");
+	QWebElement element = m_Frame->findFirstElement("#" + elementId + "-failed");
 	element.removeClass("failed");
 	element.addClass("hidden");
 }
@@ -27,7 +35,7 @@ void HtmlConsole::hideElementIndicator(QString elementId)
  */
 void HtmlConsole::showElementIndicator(QString elementId)
 {
-	QWebElement element = m_Div.findFirst("#" + elementId + "-failed");
+	QWebElement element = m_Frame->findFirstElement("#" + elementId + "-failed");
 	element.removeClass("hidden");
 	element.addClass("failed");
 }
--- 999_exe/trunk/console/html_console.h
+++ 999_exe/trunk/console/html_console.h
@@ -15,10 +15,14 @@ class HtmlConsole: public Console
 public:
 	HtmlConsole() {};
 	virtual ~HtmlConsole() {};
+	void setFrame(QWebFrame *frame);
 
 protected:
 	void hideElementIndicator(QString elementId);
 	void showElementIndicator(QString elementId);
+
+private:
+	QWebFrame *m_Frame;
 };
 
 #endif /* HTML_CONSOLE_H_ */
