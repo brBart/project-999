d54f8b7658d8f97edaf454266d9f56980da38c66
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-08-26 00:41:04 +0000

    Beginning to implement the ShowProduct class and derived.

---
+++ 999_pos/trunk/commands/show_product.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Library containing the ShowProductCommand base class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+/**
+ * For obtaining the product info.
+ */
+require_once('business/product.php');
+
+/**
+ * Defines common functionality for getting a product and show it.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+abstract class ShowProductCommand extends Command{
+	/**
+	 * Holds the request object.
+	 * @var Request
+	 */
+	protected $_mRequest;
+	
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$this->_mRequest = $request;
+		
+		try{
+			$product = $this->getProduct();
+			
+			$manufacturer = $product->getManufacturer();
+			$um = $product->getUnitOfMeasure();
+			
+			Page::display(array('id' => $id, 'name' => $product->getName(),
+					'bar_code' => $product->getBarCode(), 'packaging' => $product->getPackaging(),
+					'description' => $product->getDescription(), 'manufacturer' => $manufacturer->getName(),
+					'um' => $um->getName(), 'price' => $product->getPrice(),
+					'deactivated' => (int)$product->isDeactivated(), 'quantity' => Inventory::getQuantity($product),
+					'available' => Inventory::getAvailable($product), 'suppliers' => $product->showProductSuppliers(),
+					'lots' => Inventory::showLots($product), 'reserves' => ReserveList::getList($product)),
+					'product_info_html.tpl');
+		} catch(Exception $e){
+			$msg = $e->getMessage();
+			Page::display(array('type' => 'error', 'message' => $msg, 'notify' => '1'),
+					'product_info_html.tpl');
+		}
+	}
+	
+	/**
+	 * Gets the desired product.
+	 * @return variant
+	 */
+	abstract protected function getProduct();
+}
+?>
