aaa8db37e77c6c88ab9d1d52eba8048a07c595b6
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-09-21 22:53:40 +0000

    The AlterSessionObjectCommand class implemented.

--- 999_pos/trunk/commands/make_default_correlative.php
+++ 999_pos/trunk/commands/make_default_correlative.php
@@ -8,37 +8,33 @@
 /**
  * Base class.
  */
-require_once('presentation/command.php');
+require_once('commands/alter_session_object.php');
 /**
- * For displaying the results.
+ * Include the correlative library.
  */
-require_once('presentation/page.php');
+require_once('business/document.php');
 
 /**
  * Defines functionality for setting the correlative as the default correlative.
  * @package Command
  * @author Roberto Oliveros
  */
-class MakeDefaultCorrelativeCommand extends Command{
+class MakeDefaultCorrelativeCommand extends AlterSessionObjectCommand{
 	/**
-	 * Executes the tasks of the command.
-	 * @param Request $request
-	 * @param SessionHelper $helper
+	 * Tests if the user has the right to alter the object.
+	 * @param UserAccount $user
+	 * @return boolean
 	 */
-	public function execute(Request $request, SessionHelper $helper){
-		try{
-			$user = $helper->getUser();
-			if(AccessManager::isAllowed($user, 'correlative', 'write')){
-				$correlative = $helper->getObject((int)$request->getProperty('key'));
-				Correlative::makeDefault($correlative);
-				Page::display(array(), 'success_xml.tpl');
-			}
-			else
-				throw new Exception('Usuario no cuenta con los suficientes privilegios.');
-		} catch(Exception $e){
-			$msg = $e->getMessage();
-			Page::display(array('message' => $msg), 'error_xml.tpl');
-		}
+	protected function testRights(UserAccount $user){
+		return AccessManager::isAllowed($user, 'correlative', 'write');
+	}
+	
+	/**
+	 * Alters the desired object.
+	 * @param variant $obj
+	 */
+	protected function alterObject($obj){
+		Correlative::makeDefault($obj);
 	}
 }
 ?>
