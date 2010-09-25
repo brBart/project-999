8bd73098926fe530c35ce1513133be981805303c
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-09-25 16:24:20 +0000

    The ShowInvoiceMenuCommand class implemented.

---
+++ 999_pos/trunk/commands/show_invoice_menu.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Library containing the ShowInvoiceMenuCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+
+/**
+ * Command to display the invoice menu.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class ShowInvoiceMenuCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$back_trace = array('Inicio', 'Movimientos', 'Facturas');
+		Page::display(array('module_title' => OPERATIONS_TITLE, 'main_menu' => 'main_menu_operations_html.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'movements_menu_html.tpl',
+				'content' => 'invoice_menu_html.tpl'), 'site_html.tpl');
+	}
+}
+?>
