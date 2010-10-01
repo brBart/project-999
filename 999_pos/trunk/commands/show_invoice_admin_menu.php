9beaba2cb1b423eb2ee2820fe817b41d260c0ad9
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-10-01 16:42:44 +0000

    Back trace fixed.

--- 999_pos/trunk/commands/show_invoice_admin_menu.php
+++ 999_pos/trunk/commands/show_invoice_admin_menu.php
@@ -26,7 +26,7 @@ class ShowInvoiceAdminMenuCommand extends Command{
 	 * @param SessionHelper $helper
 	 */
 	public function execute(Request $request, SessionHelper $helper){
-		$back_trace = array('Inicio', 'Movimientos', 'Facturas');
+		$back_trace = array('Inicio', 'Caja', 'Facturas');
 		Page::display(array('module_title' => POS_ADMIN_TITLE, 'main_menu' => 'main_menu_pos_admin_html.tpl',
 				'back_trace' => $back_trace, 'second_menu' => 'cash_register_menu_html.tpl',
 				'content' => 'invoice_admin_menu_html.tpl'), 'site_html.tpl');
