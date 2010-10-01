4a5a8b9128b5248200f5a62829dc8b94906d4517
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-10-01 16:44:39 +0000

    The invoice_web_form script renamed to invoice_inventory_form

--- 999_pos/trunk/commands/get_invoice_by_working_day.php
+++ 999_pos/trunk/commands/get_invoice_by_working_day.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Library containing the GetInvoiceBySerialNumberCommand class.
+ * Library containing the GetInvoiceByWorkingDayCommand class.
  * @package Command
  * @author Roberto Oliveros
  */
@@ -26,7 +26,7 @@ require_once('business/transaction.php');
  */
 require_once('business/agent.php');
 /**
- * Fort the cash register class.
+ * Fort the working day class.
  */
 require_once('business/cash.php');
 
@@ -35,13 +35,15 @@ require_once('business/cash.php');
  * @package Command
  * @author Roberto Oliveros
  */
-class GetInvoiceBySerialNumberCommand extends GetObjectCommand{
+class GetInvoiceByWorkingDayCommand extends GetObjectCommand{
 	/**
 	 * Gets the desired object.
 	 * @return variant
 	 */
 	protected function getObject(){
-		$id = Invoice::getInvoiceId($this->_mRequest->getProperty('serial_number'),
+		$id = Invoice::getInvoiceIdByWorkingDay(
+				WorkingDay::getInstance($this->_mRequest->getProperty('working_day')),
+				$this->_mRequest->getProperty('serial_number'),
 				$this->_mRequest->getProperty('number'));
 				
 		if($id != 0)
@@ -55,16 +57,17 @@ class GetInvoiceBySerialNumberCommand extends GetObjectCommand{
 	 * @param string $msg
 	 */
 	protected function displayFailure($msg){
-		$back_trace = array('Inicio', 'Movimientos', 'Facturas');
+		$back_trace = array('Inicio', 'Caja', 'Facturas');
 		
+		$working_day = $this->_mRequest->getProperty('working_day');
 		$serial_number = $this->_mRequest->getProperty('serial_number');
 		$number = $this->_mRequest->getProperty('number');
 		
-		Page::display(array('module_title' => INVENTORY_TITLE, 'main_menu' => 'main_menu_inventory_html.tpl',
-				'back_trace' => $back_trace, 'second_menu' => 'movements_menu_html.tpl',
-				'content' => 'invoice_menu_html.tpl', 'notify' => '1',
-				'type' => 'error', 'message' => $msg, 'serial_number' => $serial_number,
-				'number' => $number), 'site_html.tpl');
+		Page::display(array('module_title' => POS_ADMIN_TITLE, 'main_menu' => 'main_menu_pos_admin_html.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'cash_register_menu_html.tpl',
+				'content' => 'invoice_admin_menu_html.tpl', 'notify' => '1',
+				'type' => 'error', 'message' => $msg, 'working_day' => $working_day,
+				'serial_number' => $serial_number, 'number' => $number), 'site_html.tpl');
 	}
 	
 	/**
@@ -74,20 +77,26 @@ class GetInvoiceBySerialNumberCommand extends GetObjectCommand{
 	 * @param array $backQuery
 	 */
 	protected function displayObject($key, $obj, $backQuery){
-		$back_trace = array('Inicio', 'Movimientos', 'Facturas');
+		$back_trace = array('Inicio', 'Caja', 'Facturas');
 		
 		// Build the back link.
 		$back_link = (is_null($backQuery)) ? 'index.php?cmd=show_invoice_menu' :
 				'index.php?cmd=' . $backQuery['cmd'] . '&page=' . $backQuery['page'] . '&start_date=' .
 				$this->_mRequest->getProperty('start_date') . '&end_date=' .
 				$this->_mRequest->getProperty('end_date');
-				
+		
+		$working_day = WorkingDay::getInstance($this->_mRequest->getProperty('working_day'));
+		$cash_register = $obj->getCashRegister();
+		$shift = $cash_register->getShift();
 		$user = $obj->getUser();
 		$correlative = $obj->getCorrelative();
 		
-		Page::display(array('module_title' => INVENTORY_TITLE, 'main_menu' => 'back_link.tpl',
+		Page::display(array('module_title' => POS_ADMIN_TITLE, 'main_menu' => 'back_link.tpl',
 				'back_trace' => $back_trace, 'second_menu' => 'none',
-				'content' => 'invoice_inventory_form_html.tpl', 'status' => $obj->getStatus(),'key' => $key,
+				'content' => 'invoice_pos_admin_form_html.tpl', 'cash_register_id' => $cashRegister->getId(),
+				'date' => $workingDay->getDate(), 'shift' => $shift->getName() . ', ' . $shift->getTimeTable(),
+				'cash_register_status' => (int)$cashRegister->isOpen(),
+				'status' => $obj->getStatus(),'key' => $key,
 				'back_link' => $back_link, 'serial_number' => $correlative->getSerialNumber(), 'number' => $obj->getNumber(),
 				'date_time' => $obj->getDateTime(), 'username' => $user->getUserName(),
 				'nit' => $obj->getCustomerNit(), 'customer' => $obj->getCustomerName()), 'site_html.tpl');
