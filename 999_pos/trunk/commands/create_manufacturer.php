30a839f11db71b56dc2e9ca8435ae4a651b75079
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-10-07 19:02:10 +0000

    Various updates.

--- 999_pos/trunk/commands/create_manufacturer.php
+++ 999_pos/trunk/commands/create_manufacturer.php
@@ -61,7 +61,7 @@ class CreateManufacturerCommand extends CreateObjectCommand{
 				'back_trace' => $back_trace, 'second_menu' => 'none',
 				'content' => 'manufacturer_form_html.tpl', 'status' => '0', 'key' => $key,
 				'back_link' => 'index.php?cmd=show_manufacturer_menu',
-				'foward_link' => 'index.php?cmd=get_manufacturer'), 'site_html.tpl');
+				'forward_link' => 'index.php?cmd=get_manufacturer'), 'site_html.tpl');
 	}
 }
 ?>
