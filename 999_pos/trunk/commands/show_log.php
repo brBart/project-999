0e16831d2aa2bf653430ab623b4c3fb171617e88
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-10-30 17:00:13 +0000

    Implementation of the invoice discounts log.

---
+++ 999_pos/trunk/commands/show_log.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Library containing the ShowLogCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+/**
+ * For user accounts validations.
+ */
+require_once('business/user_account.php');
+/**
+ * For the log objects.
+ */
+require_once('business/various.php');
+
+/**
+ * Defines common functionality for the show log command derived classes.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+abstract class ShowLogCommand extends Command{
+	/**
+	 * Holds the request object.
+	 * @var Request
+	 */
+	protected $_mRequest;
+	
+	/**
+	 * Executes the tasks of the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$this->_mRequest = $request;
+		
+		if($this->testRights($helper->getUser())){
+			$page = (int)$this->_mRequest->getProperty('page');
+			
+			try{
+				$list = $this->getList($start_date, $end_date, $total_pages, $total_items, $page);
+			} catch(Exception $e){
+				$msg = $e->getMessage();
+				$this->displayFailure($msg);
+				return;
+			}
+			
+			if($total_items > 0){
+				$first_item = (($page - 1) * ITEMS_PER_PAGE) + 1;
+				$last_item = ($page == $total_pages) ? $total_items : $page * ITEMS_PER_PAGE;
+				
+				// For back link purposes.
+				$actual_cmd = $this->_mRequest->getProperty('cmd');
+				
+				$link = 'index.php?cmd=' . $actual_cmd . '&page=';
+				$dates = '&start_date=' . $start_date . '&end_date=' . $end_date;
+				$previous_link = ($page == 1) ? '' : $link . ($page - 1) . $dates;
+				$next_link = ($page == $total_pages) ? '' : $link . ($page + 1) . $dates;
+				
+				$this->displayList($list, $start_date, $end_date, $total_pages, $total_items, $page, $first_item,
+						$last_item, $previous_link, $next_link, $actual_cmd);
+			}
+			else
+				$this->displayEmpty();
+		}
+		else{
+			$msg = 'Insuficientes privilegios.';
+			$this->displayFailure($msg);
+		}
+	}
+
+	/**
+	 * Tests if the user has the right to cancel the document.
+	 * @param UserAccount $user
+	 * @return boolean
+	 */
+	abstract protected function testRights(UserAccount $user);
+	
+	/**
+	 * Returns a list with information.
+	 * @param string &$startDate
+	 * @param string &$endDate
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
+	 * @param integer $page
+	 * @return array
+	 */
+	abstract protected function getList(&$startDate, &$endDate, &$totalPages, &$totalItems, $page);
+	
+	/**
+	 * Displays a message if something goes wrong.
+	 * @param strin $msg
+	 */
+	abstract protected function displayFailure($msg);
+	
+	/**
+	 * Displays an empty list.
+	 */
+	abstract protected function displayEmpty();
+	
+	/**
+	 * Displays the list.
+	 * @param array $list
+	 * @param string $startDate
+	 * @param string $endDate
+	 * @param integer $totalPages
+	 * @param integer $totalItems
+	 * @param integer $page
+	 * @param integer $firstItem
+	 * @param integer $lastItem
+	 * @param string $previousLink
+	 * @param string $nextLink
+	 */
+	abstract protected function displayList($list, $startDate, $endDate, $totalPages, $totalItems, $page,
+			$firstItem, $lastItem, $previousLink, $nextLink, $actualCmd);
+}
+?>
--- 999_pos/trunk/templates/log_menu_pos_admin_html.tpl
+++ 999_pos/trunk/templates/log_menu_pos_admin_html.tpl
@@ -3,16 +3,16 @@
 	<ul>
 	    <li id="li_last">
 	    	<div id="dates_range_desc">
-		    	<form method="post" action="{$search_link}"
+		    	<form method="post" action="index.php?cmd=show_invoice_discount_log&page=1"
 		    			onsubmit="return oSession.setIsLink(true);">
 		    		<div>
-		    			<label for="start_date">Descuento a facturas del</label>
-			    		<input name="start_date" id="start_date" type="text" value="{$start_date}" maxlength="10" />
+		    			<label for="discount_start_date">Descuento a facturas del</label>
+			    		<input name="discount_start_date" id="discount_start_date" type="text" value="{$discount_start_date}" maxlength="10" />
 			    		<span class="hint">dd/mm/aaaa</span>
 		    		</div>
 		    		<div>
-		    			<label for="end_date">al</label>
-			    		<input name="end_date" id="end_date" type="text" value="{$end_date}" maxlength="10" />
+		    			<label for="discount_end_date">al</label>
+			    		<input name="discount_end_date" id="discount_end_date" type="text" value="{$discount_end_date}" maxlength="10" />
 			    		<span class="hint">dd/mm/aaaa</span>
 			    		<input type="submit" value="Buscar" />
 		    		</div>
