7043527692876dd36719b67deb3a1165da7b1f7e
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-11-04 15:42:07 +0000

    Implementation of the ShowCashDocumentCancelLogCommand class.

---
+++ 999_pos/trunk/commands/show_cash_document_cancel_log.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Library containing the ShowCashDocumentCancelLogCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('commands/show_log.php');
+
+/**
+ * Implements functionality for showing the log.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class ShowCashDocumentCancelLogCommand extends ShowLogCommand{
+	/**
+	 * Returns the prefix of the date variables to use.
+	 * @return string
+	 */
+	protected function getPrefix(){
+		return 'cancel';
+	}
+	
+	/**
+	 * Tests if the user has the right to cancel.
+	 * @param UserAccount $user
+	 * @return boolean
+	 */
+	protected function testRights(UserAccount $user){
+		return AccessManager::isAllowed($user, 'cash_document_cancel_log', 'read');
+	}
+	
+	/**
+	 * Returns a list with information.
+	 * @param string $startDate
+	 * @param string $endDate
+	 * @param integer &$totalPages
+	 * @param integer &$totalItems
+	 * @param integer $page
+	 * @return array
+	 */
+	protected function getList($startDate, $endDate, &$totalPages, &$totalItems, $page){
+		return CancelCashDocumentList::getList($startDate, $endDate, $totalPages, $totalItems, $page);
+	}
+	
+	/**
+	 * Display failure in case an error occurs.
+	 * @param string $msg
+	 */
+	protected function displayFailure($msg){
+		$back_trace = array('Inicio', 'Herramientas', 'Bitacoras');
+		
+		$start_date = $this->_mRequest->getProperty('cancel_start_date');
+		$end_date = $this->_mRequest->getProperty('cancel_end_date');
+		
+		Page::display(array('module_title' => POS_AMIN_TITLE, 'main_menu' => 'main_menu_pos_admin_html.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'tools_menu_pos_admin_html.tpl',
+				'content' => 'log_menu_pos_admin_html.tpl', 'notify' => '1', 'type' => 'error',
+				'message' => $msg, 'cancel_start_date' => $start_date, 'cancel_end_date' => $end_date),
+				'site_html.tpl');
+	}
+	
+	/**
+	 * Displays an empty list.
+	 */
+	protected function displayEmpty(){
+		$back_trace = array('Inicio', 'Herramientas', 'Bitacoras');
+		$msg = 'No hay documentos que fueron anulados en esas fechas en la base de datos.';
+		Page::display(array('module_title' => POS_ADMIN_TITLE, 'main_menu' => 'back_link.tpl',
+				'back_link' => 'index.php?cmd=show_log_menu_pos_admin', 'back_trace' => $back_trace,
+				'second_menu' => 'none', 'content' => 'none', 'notify' => '1', 'type' => 'info',
+				'message' => $msg), 'site_html.tpl');
+	}
+	
+	/**
+	 * Displays the list.
+	 * @param array $list
+	 * @param string $startDate
+	 * @param string $endDate
+	 * @param integer $totalPages
+	 * @param integer $totalItems
+	 * @param integer $page
+	 * @param integer $firstItem
+	 * @param integer $lastItem
+	 * @param string $previousLink
+	 * @param string $nextLink
+	 * @param string $actualCmd
+	 */
+	protected function displayList($list, $startDate, $endDate, $totalPages, $totalItems, $page, $firstItem,
+			$lastItem, $previousLink, $nextLink, $actualCmd){
+		$back_trace = array('Inicio', 'Herramientas', 'Bitacoras');
+		Page::display(array('module_title' => POS_ADMIN_TITLE, 'main_menu' => 'back_link.tpl',
+				'back_link' => 'index.php?cmd=show_log_menu_pos_admin', 'back_trace' => $back_trace,
+				'second_menu' => 'none', 'content' => 'document_cancel_log_html.tpl', 'log_name' => 'Documentos Anulados (Caja)',
+				'list' => $list, 'start_date' => $startDate, 'end_date' => $endDate,
+				'total_items' => $totalItems, 'total_pages' => $totalPages, 'page' => $page,
+				'first_item' => $firstItem, 'last_item' => $lastItem, 'previous_link' => $previousLink,
+				'next_link' => $nextLink, 'print_cmd' => 'print_cash_document_cancel_log'), 'site_html.tpl');
+	}
+}
+?>
--- 999_pos/trunk/templates/log_menu_pos_admin_html.tpl
+++ 999_pos/trunk/templates/log_menu_pos_admin_html.tpl
@@ -1,7 +1,7 @@
 {* Smarty * }
 <div id="third_menu">
 	<ul>
-	    <li id="li_last">
+	    <li>
 	    	<div class="dates_range_desc">
 		    	<form method="post" action="index.php?cmd=show_invoice_discount_log&page=1"
 		    			onsubmit="return oSession.setIsLink(true);">
@@ -13,6 +13,24 @@
 		    		<div>
 		    			<label for="discount_end_date">al</label>
 			    		<input name="discount_end_date" id="discount_end_date" type="text" value="{$discount_end_date}" maxlength="10" />
+			    		<span class="hint">dd/mm/aaaa</span>
+			    		<input type="submit" value="Buscar" />
+		    		</div>
+		    	</form>
+	    	</div>
+	    </li>
+	    <li id="li_last">
+	    	<div class="dates_range_desc">
+		    	<form method="post" action="index.php?cmd=show_cash_document_cancel_log&page=1"
+		    			onsubmit="return oSession.setIsLink(true);">
+		    		<div>
+		    			<label for="cancel_start_date">Documentos anulados del</label>
+			    		<input name="cancel_start_date" id="cancel_start_date" type="text" value="{$cancel_start_date}" maxlength="10" />
+			    		<span class="hint">dd/mm/aaaa</span>
+		    		</div>
+		    		<div>
+		    			<label for="cancel_end_date">al</label>
+			    		<input name="cancel_end_date" id="cancel_end_date" type="text" value="{$cancel_end_date}" maxlength="10" />
 			    		<span class="hint">dd/mm/aaaa</span>
 			    		<input type="submit" value="Buscar" />
 		    		</div>
