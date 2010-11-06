69703790e718ed3db7ac1e7b5d90404742e0c8d0
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2010-11-06 00:37:27 +0000

    Implementation of the ShowTaskMenuAdminCommand class.

---
+++ 999_pos/trunk/commands/show_task_menu_admin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Library containing the ShowTaskMenuAdminCommand class.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Base class.
+ */
+require_once('presentation/command.php');
+/**
+ * For displaying the results.
+ */
+require_once('presentation/page.php');
+
+/**
+ * Command to display the task menu on the Administration site.
+ * @package Command
+ * @author Roberto Oliveros
+ */
+class ShowTaskMenuAdminCommand extends Command{
+	/**
+	 * Execute the command.
+	 * @param Request $request
+	 * @param SessionHelper $helper
+	 */
+	public function execute(Request $request, SessionHelper $helper){
+		$back_trace = array('Inicio', 'Herramientas', 'Procesos');
+		Page::display(array('module_title' => ADMIN_TITLE, 'main_menu' => 'main_menu_admin_html.tpl',
+				'back_trace' => $back_trace, 'second_menu' => 'tools_menu_admin_html.tpl',
+				'content' => 'task_menu_admin_html.tpl', 'notify' => '0'), 'site_html.tpl');
+	}
+}
+?>
---
+++ 999_pos/trunk/templates/task_menu_admin_html.tpl
@@ -0,0 +1,6 @@
+{* Smarty * }
+<div id="third_menu">
+	<ul>
+	    <li id="li_last"><a href="index.php?cmd=show_general_closure_form" onclick="return oSession.setIsLink(true);">Cierre General</a></li>
+	</ul>
+</div>
