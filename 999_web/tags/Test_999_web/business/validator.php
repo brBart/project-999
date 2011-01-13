faca1a57ceffb11686482e92c26b3c326d4ac8f7
robertooliveros@mac.com <robertooliveros@mac.com@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2011-01-13 16:50:20 +0000

    This commit was manufactured by cvs2svn to create tag 'Test_999_web'.

---
+++ 999_web/tags/Test_999_web/business/validator.php
@@ -0,0 +1,367 @@
+<?php
+/**
+ * Library with utility class with validation or other routines.
+ * @package Validator
+ * @author Roberto Oliveros
+ */
+
+/**
+ * Class that represents an exception when a validation has failed.
+ * @package Validator
+ * @author Roberto Oliveros
+ */
+class ValidateException extends Exception{
+	/**
+	 * Holds the name of the property.
+	 * @var string
+	 */
+	private $_mProperty;
+	
+	/**
+	 * Constructs the exception object with the property name.
+	 * @param string $property
+	 * @param string $msg
+	 */
+	public function __construct($msg, $property = NULL){
+		parent::__construct($msg);
+		
+		$this->_mProperty = $property;
+	}
+	
+	/**
+	 * Returns the property name.
+	 * @return string
+	 */
+	public function getProperty(){
+		return $this->_mProperty;
+	}
+}
+
+/**
+ * Class with needed routines regarding dates.
+ * @package Validator
+ * @author Roberto Oliveros
+ */
+class Date{
+	/**
+	 * Validates the provided date.
+	 *
+	 * Verifies if it is a valid date. Otherwise it throws a validate exception.
+	 * @param string $date
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateDate($date, $msg, $property = NULL){
+		$date_array = explode('/', $date);
+		
+		$day = (int)$date_array[0];
+		$month = (int)$date_array[1];
+		$year = (int)$date_array[2];
+		
+		if(!checkdate($month, $day, $year))
+			throw new ValidateException($msg . ' No existe o debe ser en formato \'dd/mm/aaaa\'.', $property);
+	}
+	
+	/**
+	 * Validates the provided datetime value.
+	 *
+	 * Verifies if it is a valid date and time. Otherwise it throws a validate exception.
+	 * @param string $dateTime
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateDateTime($dateTime, $msg, $property = NULL){
+		$date_array = explode(' ', $dateTime);
+		self::validateDate($date_array[0], $msg);
+		
+		if(!preg_match('/([0-1]\d|2[0-3]):([0-5]\d):([0-5]\d)/', $date_array[1]))
+			throw new ValidateException($msg . ' Formato de hora debe ser HH::MM:SS', $property);
+	}
+	
+	/**
+	 * Compares if the last date is greater than the first date.
+	 *
+	 * Returns true if it is the case.
+	 * @param string $firstDate
+	 * @param string $lastDate
+	 * @return boolean
+	 */
+	static public function compareDates($firstDate, $lastDate){
+		$first_date = Date::englishFormat($firstDate);
+		$last_date = Date::englishFormat($lastDate);
+		
+		$nix_first_date = strtotime($first_date);
+		$nix_last_date = strtotime($last_date);
+		
+		if($nix_first_date < $nix_last_date)
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * Change the dates format 'dd/mm/yyyy' to a database format 'yyyy/mm/dd'.
+	 *
+	 * @param string $date
+	 * @return string
+	 */
+	static public function dbFormat($date){
+		$date_array = explode('/', $date);
+		
+		$db_date_array = array($date_array[2], $date_array[1], $date_array[0]);
+		
+		return implode('/', $db_date_array);
+	}
+	
+	/**
+	 * Change the dates format 'dd/mm/yyyy' to a database format 'yyyy/mm/dd'.
+	 *
+	 * @param string $dateTime
+	 * @return string
+	 */
+	static public function dbDateTimeFormat($dateTime){
+		$date_array = explode(' ', $dateTime);		
+		return self::dbFormat($date_array[0]) . ' ' . $date_array[1];
+	}
+	
+	/**
+	 * Adds a cero to the left side in case the day or month values contains only 1 digit.
+	 *
+	 * Eg: 1/1/2009 becomes 01/01/2009.
+	 * @param string $date
+	 * @return string
+	 */
+	static public function paddingDate($date){
+		$date_array = explode('/', $date);
+		
+		$db_date_array = array(sprintf('%02s', $date_array[0]), sprintf('%02s', $date_array[1]),
+				$date_array[2]);
+		
+		return implode('/', $db_date_array);
+	}
+	
+	/**
+	 * Change the dates format 'dd/mm/yyyy' to an english format 'mm/dd/yyyy'.
+	 *
+	 * @param string $date
+	 * @return string
+	 */
+	static private function englishFormat($date){
+		$date_array = explode('/', $date);
+		
+		$eng_date_array = array($date_array[1], $date_array[0], $date_array[2]);
+		
+		return implode('/', $eng_date_array);
+	}
+}
+
+
+/**
+ * Class for validating values.
+ * @package Validator
+ * @author Roberto Oliveros
+ */
+class Number{
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be an integer. Otherwise it throws a validate exception. The msg parameter is for displaying the
+	 * desire message.
+	 * @param integer $quantity
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateInteger($number, $msg, $property = NULL){
+		if(!is_int($number))
+			throw new ValidateException($msg . ' Valor debe ser numerico.', $property);
+	}
+	
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be greater than cero. Otherwise it throws a validate exception. The msg parameter is for displaying
+	 * the desire message.
+	 * @param integer $number
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validatePositiveInteger($number, $msg, $property = NULL){
+		if(!is_int($number) || $number < 1)
+			throw new ValidateException($msg . ' Valor debe ser mayor que cero.', $property);
+	}
+	
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be greater or equal to cero. Otherwise it throws a validate exception. The msg parameter is for
+	 * displaying the desire message.
+	 * @param integer $number
+	 * @param string $msg
+	 * @param string $property
+	 * @throws Exception
+	 */
+	static public function validateUnsignedInteger($number, $msg, $property = NULL){
+		if(!is_int($number) || $number < 0)
+			throw new ValidateException($msg . ' Valor no debe ser menor que cero.', $property);
+	}
+	
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be a float number. The msg parameter is for displaying the desire
+	 * message.
+	 * @param float $number
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateFloat($number, $msg, $property = NULL){
+		if(!is_float($number))
+			throw new ValidateException($msg . ' Valor debe ser numerico.', $property);
+	}
+	
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be greater than cero. Otherwise it throws a validate exception. The msg parameter is for displaying
+	 * the desire message.
+	 * @param float $number
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validatePositiveFloat($number, $msg, $property = NULL){
+		if(!is_float($number) || $number < 1)
+			throw new ValidateException($msg . ' Valor no debe ser menor que cero.', $property);
+	}
+	
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be greater or equal to cero. Otherwise it throws a validate exception. The msg parameter is for
+	 * displaying the desire message.
+	 * @param float $number
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateUnsignedFloat($number, $msg, $property = NULL){
+		if(!is_float($number) || $number < 0)
+			throw new ValidateException($msg . ' Valor no debe ser menor que cero.', $property);
+	}
+	
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be numeric. Otherwise it throws a validate exception. The msg parameter is for displaying the
+	 * desire message.
+	 * @param integer $quantity
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateNumber($number, $msg, $property = NULL){
+		if(!is_numeric($number))
+			throw new ValidateException($msg . ' Valor debe ser numerico.', $property);
+	}
+	
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be greater than cero. Otherwise it throws a validate exception. The msg parameter is for displaying
+	 * the desire message.
+	 * @param string $number
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validatePositiveNumber($number, $msg, $property = NULL){
+		if(!is_numeric($number) || $number < 1)
+			throw new ValidateException($msg . ' Valor debe ser mayor que cero.', $property);
+	}
+	
+	/**
+	 * Validates the provided number.
+	 *
+	 * Must be greater or equal to cero. Otherwise it throws a validate exception. The msg parameter is for
+	 * displaying the desire message.
+	 * @param string $number
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateUnsignedNumber($number, $msg, $property = NULL){
+		if(!is_numeric($number) || $number < 0)
+			throw new ValidateException($msg . ' Valor no debe ser menor que cero.', $property);
+	}
+	
+	/**
+	 * Validates the provided number if it is between 0 to 100.
+	 *
+	 * Must be greater or equal to cero and less or equal to 100. Otherwise it throws a validate exception.
+	 * The msg parameter is for displaying the desire message.
+	 * @param string $number
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateBetweenCeroToOneHundredNumber($number, $msg, $property = NULL){
+		if(!is_numeric($number) || ($number < 0 || $number > 100))
+			throw new ValidateException($msg . ' Valor no debe ser menor que cero ni mayor que cien.',
+					$property);
+	}
+}
+
+
+/**
+ * Class for validating string values.
+ * @package Validator
+ * @author Roberto Oliveros
+ */
+class String{
+	/**
+	 * Validates a normal string.
+	 *
+	 * Must not be empty. The msg parameter is for displaying the desire
+	 * message.
+	 * @param string $string
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateString($string, $msg, $property = NULL){
+		if($string == '')
+			throw new ValidateException($msg . ' Valor no puede ser vacio.', $property);
+	}
+	
+	/**
+	 * Validates if string is composed by only alphanumeric characters.
+	 *
+	 * The msg parameter is for displaying the desire message.
+	 * @param string $string
+	 * @param string $msg
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateAlphanum($string, $msg, $property = NULL){
+		if(!ctype_alnum($string))
+			throw new ValidateException($msg . ' Valor solo puede ser alfanum&eacute;rico.', $property);
+	}
+	
+	/**
+	 * Validates the provided nit.
+	 * 
+	 * Verifies that the nit is set correctly, e.g. 1725045-5. Otherwise it throws a validate exception. The msg
+	 * parameter is for displaying the desire message.
+	 * @param string $nit
+	 * @param string $property
+	 * @throws ValidateException
+	 */
+	static public function validateNit($nit, $msg, $property = NULL){
+		if(!preg_match('/^[0-9]+[-][0-9]$/', $nit))
+			throw new ValidateException($msg . ' Formato debe ser ######-#', $property);
+	}
+}
+?>
---
+++ 999_web/tags/Test_999_web/data/inventory_dam.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * Library with utility classes for accessing inventory related tables in the database.
+ * @package InventoryDAM
+ * @author Roberto Oliveros
+ */
+
+/**
+ * For accessing the database.
+ */
+require_once('data/database_handler.php');
+
+/**
+ * Utility class for accessing comparison data in the database.
+ * @package InventoryDAM
+ * @author Roberto Oliveros
+ */
+class ComparisonDAM{
+	/**
+	 * Returns a comparison with the details corresponding to the requested page.
+	 *
+	 * The total_pages and total_items arguments are necessary to return their respective values. Returns NULL
+	 * if there was no match for the provided id in the database. 
+	 * @param integer $id
+	 * @return Comparison
+	 */
+	static public function getInstance($id){
+		$sql = 'CALL comparison_get(:comparison_id)';
+		$params = array(':comparison_id' => $id);
+		$result = DatabaseHandler::getRow($sql, $params);
+		
+		if(!empty($result)){
+			$sql = 'CALL comparison_product_get(:comparison_id)';
+			$items_result = DatabaseHandler::getAll($sql, $params);
+			
+			$details = array();
+			foreach($items_result as $detail){
+				$product = Product::getInstance((int)$detail['product_id']);
+				$details[] = new ComparisonDetail($product, (int)$detail['physical'], (int)$detail['system']);
+			}
+			
+			$user = UserAccount::getInstance($result['user_account_username']);
+			
+			return new Comparison($id, $result['created_date'], $user, $result['reason'],
+					(boolean)$result['general'], $details, (int)$result['physical_total'],
+					(int)$result['system_total']);
+		}
+		else
+			return NULL;
+	}
+	
+	/**
+	 * Creates a comparison in the database and returns the new created comparison's id.
+	 *
+	 * @param string $date
+	 * @param UserAccount $user
+	 * @param Count $count
+	 * @param string $reason
+	 * @param boolean $general
+	 * @return integer
+	 */
+	static public function insert($date, UserAccount $user, Count $count, $reason, $general){
+		$sql = 'CALL comparison_insert(:username, :date, :reason, :general, :physical_total)';
+		$params = array(':username' => $user->getUserName(),
+				':date' => Date::dbDateTimeFormat($date), ':reason' => $reason,
+				':general' => (int)$general, ':physical_total' => $count->getTotal());
+		DatabaseHandler::execute($sql, $params);
+		
+		$sql = 'CALL get_last_insert_id()';
+		$id = (int)DatabaseHandler::getOne($sql);
+		
+		if($general)
+			$sql = 'CALL comparison_product_general_insert(:comparison_id, :count_id)';
+		else
+			$sql = 'CALL comparison_product_insert(:comparison_id, :count_id)';
+			
+		$params = array(':comparison_id' => $id, ':count_id' => $count->getId());
+		DatabaseHandler::execute($sql, $params);
+		
+		return $id;
+	}
+}
+
+
+/**
+ * Utility class to manipulate the detail data in the database.
+ * @package InventoryDAM
+ * @author Roberto Oliveros
+ */
+class CountDetailDAM{
+	/**
+	 * Inserts the detail's data in the database.
+	 *
+	 * @param Count $count
+	 * @param CountDetail $detail
+	 */
+	static public function insert(Count $count, CountDetail $detail){
+		$sql = 'CALL count_product_insert(:count_id, :product_id, :quantity)';
+		$product = $detail->getProduct();
+		$params = array(':count_id' => $count->getId(), ':product_id' => $product->getId(),
+				':quantity' => $detail->getQuantity());
+		DatabaseHandler::execute($sql, $params);
+	}
+	
+	/**
+	 * Deletes the provided detail from the database.
+	 *
+	 * @param Count $count
+	 * @param CountDetail $detail
+	 */
+	static public function delete(Count $count, CountDetail $detail){
+		$sql = 'CALL count_product_delete(:count_id, :product_id)';
+		$product = $detail->getProduct();
+		$params = array(':count_id' => $count->getId(), ':product_id' => $product->getId());
+		DatabaseHandler::execute($sql, $params);
+	}
+}
+
+
+/**
+ * Utility class for manipulate counts' data in the database.
+ * @package InventoryDAM
+ * @author Roberto Oliveros
+ */
+class CountDAM{
+	/**
+	 * Returns an instance of a count.
+	 *
+	 * Returns NULL if there was no match for the provided id in the database.
+	 * @param integer $id
+	 * @return Count
+	 */
+	static public function getInstance($id){
+		$sql = 'CALL count_get(:count_id)';
+		$params = array(':count_id' => $id);
+		$result = DatabaseHandler::getRow($sql, $params);
+		
+		if(!empty($result)){
+			$user = UserAccount::getInstance('roboli');
+			$count = new Count($id, $result['created_date'], $user, Persist::CREATED);
+			
+			$sql = 'CALL count_product_get(:count_id)';
+			$items_result = DatabaseHandler::getAll($sql, $params);
+			
+			$details = array();
+			foreach($items_result as $detail){
+				$product = Product::getInstance((int)$detail['product_id']);
+				$details[] = new CountDetail($product, (int)$detail['quantity'], Persist::CREATED);
+			}
+			
+			$count->setData($result['reason'], (int)$result['total'], $details);
+			return $count;
+		}
+		else
+			return NULL;
+	}
+	
+	/**
+	 * Inserts the count's data in the database.
+	 *
+	 * Returns the new created id from the database.
+	 * @param Count $obj
+	 * @return integer
+	 */
+	static public function insert(Count $obj){
+		$sql = 'CALL count_insert(:username, :date, :reason, :total)';
+		$user = $obj->getUser();
+		$params = array(':username' => $user->getUserName(),
+				':date' => Date::dbDateTimeFormat($obj->getDateTime()), ':reason' => $obj->getReason(),
+				':total' => $obj->getTotal());
+		DatabaseHandler::execute($sql, $params);
+		
+		$sql = 'CALL get_last_insert_id()';
+		return (int)DatabaseHandler::getOne($sql);
+	}
+	
+	/**
+	 * Updates the count's data in the database.
+	 *
+	 * @param Count $obj
+	 */
+	static public function update(Count $obj){
+		$sql = 'CALL count_update(:count_id, :total)';
+		$params = array(':count_id' => $obj->getId(), ':total' => $obj->getTotal());
+		DatabaseHandler::execute($sql, $params);
+	}
+	
+	/**
+	 * Deletes the count from the database.
+	 *
+	 * @param Count $obj
+	 */
+	static public function delete(Count $obj){
+		$sql = 'CALL count_delete(:count_id)';
+		$params = array(':count_id' => $obj->getId());
+		DatabaseHandler::execute($sql, $params);
+	}
+}
+
+
+/**
+ * Utility class for recolecting the necessary data from the database for printing a counting template.
+ * @package InventoryDAM
+ * @author Roberto Oliveros
+ */
+class CountingTemplateDAM{
+	/**
+	 * Returns an array with the necessary data for printing the template ordered by product name.
+	 *
+	 * The array's fields are product_id, bar_code, manufacturer, name and packaging.
+	 * @param string $first
+	 * @param string $last
+	 * @return array
+	 */
+	static public function getDataByProduct($first, $last){
+		$sql = 'CALL product_counting_template_get(:first, :last)';
+		$params = array(':first' => $first, ':last' => $last);
+		return DatabaseHandler::getAll($sql, $params);
+	}
+	
+	/**
+	 * Returns an array with the necessary data for printing the template ordered by manufacturer name.
+	 *
+	 * The array's fields are product_id, bar_code, manufacturer, name and packaging.
+	 * @param string $first
+	 * @param string $last
+	 * @return array
+	 */
+	static public function getDataByManufacturer($first, $last){
+		$sql = 'CALL manufacturer_counting_template_get(:first, :last)';
+		$params = array(':first' => $first, ':last' => $last);
+		return DatabaseHandler::getAll($sql, $params);
+	}
+}
+?>
