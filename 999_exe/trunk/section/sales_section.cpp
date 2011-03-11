3de7baa7ad6c5d32093d15f33a1ab59a29ad047d
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2011-03-11 02:21:15 +0000

    Uninitialized pointer problem.

--- 999_exe/trunk/section/sales_section.cpp
+++ 999_exe/trunk/section/sales_section.cpp
@@ -33,7 +33,7 @@ SalesSection::SalesSection(QNetworkCookieJar *jar, QWebPluginFactory *factory,
 		QUrl *serverUrl, QString cashRegisterKey, QWidget *parent)
 		: DocumentSection(jar, factory, serverUrl, cashRegisterKey, parent)
 {
-
+	m_ProductModel = 0;
 }
 
 /**
