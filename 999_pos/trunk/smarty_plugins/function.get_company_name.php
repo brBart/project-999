134a8ac060308162fd10062ff182c48c1e9519a4
roboli <roboli@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2011-06-01 23:24:46 +0000

    Documentation fix.

--- 999_pos/trunk/smarty_plugins/function.get_company_name.php
+++ 999_pos/trunk/smarty_plugins/function.get_company_name.php
@@ -11,7 +11,7 @@
 require_once('business/various.php');
 
 /**
- * Smarty plugin for getting the actual username.
+ * Smarty plugin for getting the company's name.
  */
 function smarty_function_get_company_name($params, &$smarty)
 {
