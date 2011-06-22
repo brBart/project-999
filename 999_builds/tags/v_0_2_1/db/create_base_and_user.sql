150356c4ddc33113e8f54b9231a5bbee1a1a92fc
robertooliveros@mac.com <robertooliveros@mac.com@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2011-06-22 21:18:37 +0000

    This commit was manufactured by cvs2svn to create tag 'v_0_2_1'.

---
+++ 999_builds/tags/v_0_2_1/db/create_base_and_user.sql
@@ -0,0 +1,16 @@
+-- DELIMITER $$
+
+--
+-- Base de datos: `@db_database@`
+--
+CREATE DATABASE `@db_database@` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci$$
+USE `@db_database@`$$
+
+--
+-- Create @db_user@
+--
+GRANT ALL PRIVILEGES ON `@db_database@`.*
+
+      TO '@db_user@'@'localhost' IDENTIFIED BY '@db_password@'
+
+      WITH GRANT OPTION$$
