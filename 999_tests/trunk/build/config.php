20a0bb236d508a3a56896075a875df947a9bb17d
robertooliveros@mac.com <robertooliveros@mac.com@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2011-12-16 18:16:15 +0000

    Automatic build with phing implemented.

--- 999_tests/trunk/build/config.php
+++ 999_tests/trunk/build/config.php
@@ -2,12 +2,12 @@
 define('ITEMS_PER_PAGE', 4);
 define('PRODUCTS_PER_PAGE', 4);
 
-define('MYSQL_DIR', 'C:/xampp/mysql/bin/');
+define('MYSQL_DIR', '@mysql_dir@');
 define('DB_PERSISTENCY', 'true');
-define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', '999_user');
-define('DB_PASSWORD', '999_user');
-define('DB_DATABASE', '999_store');
+define('DB_SERVER', '@db_server@');
+define('DB_USERNAME', '@db_user@');
+define('DB_PASSWORD', '@db_password@');
+define('DB_DATABASE', '@db_database@');
 define('PDO_DSN', 'mysql:host=' . DB_SERVER . ';dbname=' . DB_DATABASE);
 
 define('CORRELATIVE_VALID_DAYS', 10);
@@ -18,4 +18,7 @@ define('BACKUP_DIR', SITE_ROOT . BACKUP_DIR_NAME);
 define('SALES_LEDGER_DIR_NAME', '/sales_ledger/');
 define('SALES_LEDGER_DIR', SITE_ROOT . SALES_LEDGER_DIR_NAME);
 define('COMMANDS_DIR', SITE_ROOT . '/commands/');
+
+define('LIBS_999', '@libs_dir@');
+set_include_path(get_include_path() . ';' . LIBS_999 . ';' . SITE_ROOT);
 ?>
