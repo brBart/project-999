1af4ba97e5f73f2fc7230f10377b131193c3a4b6
robertooliveros@mac.com <robertooliveros@mac.com@ab683c2c-83f1-0251-f5a9-ccc330e75ba8>
2012-03-05 19:18:30 +0000

    Double semi colon removed.

--- 999_pos/trunk/commands/get_comparison.php
+++ 999_pos/trunk/commands/get_comparison.php
@@ -69,7 +69,7 @@ class GetComparisonCommand extends GetObjectCommand{
 		$forward_link = 'index.php?cmd=get_comparison';
 		$forward_link .= (is_null($backQuery)) ? '' : '&last_cmd=' . $backQuery['cmd'] . '&page=' .
 				$backQuery['page'] . '&start_date=' . $this->_mRequest->getProperty('start_date') .
-				'&end_date=' . $this->_mRequest->getProperty('end_date');;
+				'&end_date=' . $this->_mRequest->getProperty('end_date');
 				
 		$user = $obj->getUser();
 		
